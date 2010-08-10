cd515fd33351ea0bc94ace30e549b35ca75916ab
njpatel <>
2010-08-10 13:01:10 +0100

    Add Chase's name to the authors
    (bzr r424.1.9)

--- unity-private/gesture/gesture-dispatcher-xcb-glue.c
+++ unity-private/gesture/gesture-dispatcher-xcb-glue.c
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Authored by Neil Jagdish Patel <neil.patel@canonical.com>
+ *             Chase Douglas <chase.douglas@canonical.com>
  *
  */
 
