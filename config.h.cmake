e275aa54b9bccd000c78af254742442a8c7999c0
Neil Jagdish Patel <neil.patel@canonical.com>
2010-11-02 22:51:08 +0000

    Build and install libunity and it's header
    (bzr r572.1.4)

---
+++ config.h.cmake
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#cmakedefine PREFIXDIR "@PREFIXDIR@"
+#cmakedefine DATADIR "@DATADIR@"
+#cmakedefine PKGDATADIR "@PKGDATADIR@"
+#cmakedefine LOCALE_DIR "@LOCALE_DIR@"
+#cmakedefine VERSION "@VERSION@"
+
+#endif // CONFIG_H
