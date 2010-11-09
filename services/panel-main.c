fa14cd0dea4239bdec815117e1120ecf695dc861
Neil Jagdish Patel <neil.patel@canonical.com>
2010-11-09 19:04:40 +0000

    Initial service bits
    (bzr r572.7.2)

---
+++ services/panel-main.c
@@ -0,0 +1,16 @@
+
+#include <glib.h>
+#include <gio/gio.h>
+#include <gtk/gtk.h>
+
+#include <libindicator/indicator.h>
+#include <libindicator/indicator-object.h>
+
+gint
+main (gint argc, gchar **argv)
+{
+  g_print ("hello\n");
+
+
+  return 0;
+}
