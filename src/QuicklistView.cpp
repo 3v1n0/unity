d92415730d4dffa967654c4696742dedfb39abb1
Mirco Müller <mirco.mueller@ubuntu.com>
2010-11-30 12:18:49 +0100

    really don't call FillInDefaultItems()
    (bzr r636.6.5)

--- src/QuicklistView.cpp
+++ src/QuicklistView.cpp
@@ -72,8 +72,6 @@ QuicklistView::QuicklistView ()
   
   _vlayout->AddLayout (_default_item_layout, 0);
 
-  //FillInDefaultItems ();  
-
   _vlayout->AddLayout (_bottom_space, 0);
 
   _hlayout->AddLayout (_left_space, 0);
