55cc13439fc56c73dcaf44f6576ddf98ef04a11d
Sebastien Bacher <seb128@ubuntu.com>
2010-12-09 17:45:46 +0100

    * debian/source_unity.py:   - reassign crashes due to the indicators to the right source directly
    (bzr r55.3.274)

--- debian/source_unity.py
+++ debian/source_unity.py
@@ -1,5 +1,12 @@
-import os, apport.packaging, apport.hookutils
+import apport.packaging
 
 def add_info(report, ui):
+	# the crash is not in the unity code so reassign
+	if report.has_key("Stacktrace") and "/usr/lib/indicators" in report["Stacktrace"]:
+		for words in report["Stacktrace"].split():
+			if words.startswith("/usr/lib/indicators"):
+				report.add_package_info(apport.packaging.get_file_package(words))
+				return
+
 	report.add_hooks_info(ui, srcpackage='compiz')
 
