80feb174bb2b52951d235e35a733ad7410625529
Didier Roche <didier.roche@canonical.com>
2011-01-20 13:32:19 +0100

    * debian/source_unity.py:   - enhance apport hook to report compiz (and xorg) information only on     graphical bugs (the report can take time and would upload too many data for     just a weird quick behavior bug for instance)
    (bzr r55.3.310)

--- debian/source_unity.py
+++ debian/source_unity.py
@@ -1,12 +1,26 @@
 import apport.packaging
 
 def add_info(report, ui):
-	# the crash is not in the unity code so reassign
-	if report.has_key("Stacktrace") and "/usr/lib/indicators" in report["Stacktrace"]:
-		for words in report["Stacktrace"].split():
-			if words.startswith("/usr/lib/indicators"):
-				report.add_package_info(apport.packaging.get_file_package(words))
-				return
+    # the crash is not in the unity code so reassign
+    if report.has_key("Stacktrace") and "/usr/lib/indicators" in report["Stacktrace"]:
+        for words in report["Stacktrace"].split():
+            if words.startswith("/usr/lib/indicators"):
+                report.add_package_info(apport.packaging.get_file_package(words))
+                return
 
-	report.add_hooks_info(ui, srcpackage='compiz')
+    # only reports all compiz infos if a graphical bug
+    report_compiz = True
+    if ui:
+        if not ui.yesno("Thanks for reporting this bug on unity.  Is the issue you are reporting is graphical (will report more information about your graphic configuration)?"):
+            report_compiz = False
+    if report_compiz:
+        report.add_hooks_info(ui, srcpackage='compiz')
+    else:
+        # still send some info like the plugins activated
+        # Plugins
+        report['CompizPlugins'] = command_output(['gconftool-2',
+            '--get', '/apps/compiz-1/general/screen0/options/active_plugins'])
+
+        # User configuration
+        report['GconfCompiz'] = command_output(['gconftool-2', '-R', '/apps/compiz-1'])
 
