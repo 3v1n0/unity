737ee8765154e3b3d269c0f08502b64da89bb839
Marco Trevisan (Treviño) <mail@3v1n0.net>
2011-01-27 00:43:00 +0100

    panel-service: emit "scroll-entry" signal on scroll over an entry
    unity (panel-service, in fact): now sends also a "scroll-entry"
    signal when an indicator is scrolled, with the target entry as
    parameter.
    
    (bzr r785.8.1)

--- services/panel-service.c
+++ services/panel-service.c
@@ -1005,7 +1005,9 @@ panel_service_scroll_entry (PanelService   *self,
   IndicatorObjectEntry *entry = g_hash_table_lookup (priv->id2entry_hash, entry_id);
   IndicatorObject *object = g_hash_table_lookup (priv->entry2indicator_hash, entry);
   GdkScrollDirection direction = delta > 0 ? GDK_SCROLL_DOWN : GDK_SCROLL_UP;
+
   g_signal_emit_by_name(object, "scroll", abs(delta/120), direction);
+  g_signal_emit_by_name(object, "scroll-entry", entry, abs(delta/120), direction);
 }
 
 void
