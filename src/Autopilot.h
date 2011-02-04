b4d41b582fc7ea42126865f54a05c15df43f479d
Alex Launi <alex.launi@canonical.com>
2011-02-04 12:18:42 -0500

    fix copyright
    (bzr r738.2.35)

--- src/Autopilot.h
+++ src/Autopilot.h
@@ -1,6 +1,6 @@
 // -*- Mode: C++; indent-tabs-mode: nil; tab-width: 2 -*-
 /*
- * Copyright (C) 2010 Canonical Ltd
+ * Copyright (C) 2011 Canonical Ltd
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Authored by: Alex Launi <alex.launi@gmail.com>
+ * Authored by: Alex Launi <alex.launi@canonical.com>
  */
 
 #ifndef _AUTOPILOT_H
