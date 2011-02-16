633d8f11b54207124020ffc8d8f4da4a498f63cb
Tim Penhey <tim.penhey@canonical.com>
2011-02-16 21:56:09 +1300

    First hack at the interface for a variant builder helper.
    (bzr r861.1.1)

---
+++ src/VariantBuilder.h
@@ -0,0 +1,44 @@
+// -*- Mode: C++; indent-tabs-mode: nil; tab-width: 2 -*-
+/*
+ * Copyright (C) 2011 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Tim Penhey <tim.penhey@canonical.com>
+ */
+
+#ifndef VARIANTBUILDER_H
+#define VARIANTBUILDER_H
+
+#include <glib.h>
+
+namespace unity {
+namespace variant {
+
+class BuilderWrapper
+{
+public:
+  BuilderWrapper(GVariantBuilder* builder);
+
+  BuilderWrapper& add(char const* name, bool value);
+  BuilderWrapper& add(char const* name, char const* value);
+  BuilderWrapper& add(char const* name, int value);
+
+private:
+  GVariantBuilder* builder_;
+};
+
+}
+}
+
+#endif
