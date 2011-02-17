3d66b096f54b1ccaf74fbab5ca75e3cd106dd1eb
Didier Roche <didier.roche@canonical.com>
2011-02-17 13:12:38 +0100

    debian/source_unity.py: enhanced apport hook for incoming compiz hook
    (bzr r55.3.337)

--- debian/source_unity.py
+++ debian/source_unity.py
@@ -1,6 +1,7 @@
 import apport.packaging
 
 def add_info(report, ui):
+    
     # the crash is not in the unity code so reassign
     if report.has_key("Stacktrace") and "/usr/lib/indicators" in report["Stacktrace"]:
         for words in report["Stacktrace"].split():
@@ -9,11 +10,11 @@ def add_info(report, ui):
                 return
 
     # only reports all compiz infos if a graphical bug
-    report_compiz = True
-    if ui:
-        if not ui.yesno("Thanks for reporting this bug on unity.  Is the issue you are reporting graphical (will report more information about your graphic configuration)?"):
-            report_compiz = False
-    if report_compiz:
+    compiz_bug = False
+    if ui and report['SourcePackage'] == "unity":
+        if ui.yesno("Thanks for reporting this bug on unity.  Is the issue you are reporting graphical (will report more information about your graphic configuration)?"):
+            compiz_bug = True
+    if compiz_bug:
         report.add_hooks_info(ui, srcpackage='compiz')
     else:
         # still send some info like the plugins activated
@@ -23,4 +24,3 @@ def add_info(report, ui):
 
         # User configuration
         report['GconfCompiz'] = command_output(['gconftool-2', '-R', '/apps/compiz-1'])
-
