095f47af98908939ecae2187435bd71752749047
Neil Jagdish Patel <neil.patel@canonical.com>
2011-03-17 13:56:52 +0000

    SIGSEGV in PlaceEntryHome::SetSearch
    Fixes LP: #731927
    
    (bzr r961.2.6)

--- src/PlaceEntryHome.cpp
+++ src/PlaceEntryHome.cpp
@@ -21,6 +21,7 @@
 #include "PlaceEntryHome.h"
 
 #include <glib/gi18n-lib.h>
+#include <algorithm>
 
 class PlaceEntryGroupHome : public PlaceEntryGroup
 {
@@ -94,6 +95,20 @@ PlaceEntryHome::OnPlaceAdded (Place *place)
   {
     PlaceEntry *entry = static_cast<PlaceEntry *> (*i);
     OnPlaceEntryAdded (entry);
+  }
+
+  place->entry_removed.connect (sigc::mem_fun (this, &PlaceEntryHome::OnPlaceEntryRemoved));
+}
+
+void
+PlaceEntryHome::OnPlaceEntryRemoved (PlaceEntry *entry)
+{
+  std::vector<PlaceEntry *>::iterator it;
+
+  it = std::find (_entries.begin (), _entries.end (), entry);
+  if (it != _entries.end ())
+  {
+    _entries.erase (it);
   }
 }
 
