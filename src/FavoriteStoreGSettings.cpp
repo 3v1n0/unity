eec5f4017bd8ff0727bb5d3994af62d23f070038
Tim Penhey <tim.penhey@canonical.com>
2011-05-18 15:35:42 +1200

    Adding some glib wrappers, and updating FavoriteStore to use strings.
    (bzr r1187.5.1)

--- src/FavoriteStoreGSettings.cpp
+++ src/FavoriteStoreGSettings.cpp
@@ -17,71 +17,79 @@
 * Authored by: Neil Jagdish Patel <neil.patel@canonical.com>
 */
 
-#include "config.h"
+#include "FavoriteStoreGSettings.h"
+
+#include <iostream>
 
 #include <gio/gdesktopappinfo.h>
 
-#include "FavoriteStoreGSettings.h"
+#include "config.h"
 
-#define SETTINGS_NAME "com.canonical.Unity.Launcher"
+namespace unity {
+namespace internal {
 
-#define LATEST_SETTINGS_MIGRATION "3.2.10"
+namespace {
 
-static void on_settings_updated (GSettings *settings, const gchar *key, FavoriteStoreGSettings *self);
+const char* SETTINGS_NAME "com.canonical.Unity.Launcher";
+const char* LATEST_SETTINGS_MIGRATION "3.2.10";
 
-FavoriteStoreGSettings::FavoriteStoreGSettings ()
+void on_settings_updated(GSettings* settings,
+                         const gchar* key,
+                         FavoriteStoreGSettings* self)
 {
-  m_settings = g_settings_new (SETTINGS_NAME);
+  if (settings and key) {
+    self->Changed(key);
+  }
+}
 
+}
+
+FavoriteStoreGSettings::FavoriteStoreGSettings()
+  : settings_(g_settings_new(SETTINGS_NAME))
+  , ignore_signals_(false)
+{
   Init ();
 }
 
-FavoriteStoreGSettings::FavoriteStoreGSettings (GSettingsBackend *backend)
+FavoriteStoreGSettings::FavoriteStoreGSettings(GSettingsBackend *backend)
+  : settings_(g_settings_new_with_backend(SETTINGS_NAME, backend));
+  , ignore_signals_(false)
 {
-  m_settings = g_settings_new_with_backend (SETTINGS_NAME, backend);
-
   Init ();
 }
 
-void
-FavoriteStoreGSettings::Init ()
+void FavoriteStoreGSettings::Init()
 {
-  gchar *latest_migration_update;
-
-  m_favorites = NULL;
-  m_ignore_signals = false;
-
   /* migrate the favorites if needed and ignore errors */
-  latest_migration_update = g_settings_get_string (m_settings, "favorite-migration");
-  if (g_strcmp0 (latest_migration_update, LATEST_SETTINGS_MIGRATION) < 0)
-    {
-      GError *error = NULL;
-      char *cmd = g_strdup_printf ("%s/lib/unity/migrate_favorites.py", PREFIXDIR);
-      char *output = NULL;
-      
-      g_spawn_command_line_sync (cmd, &output, NULL, NULL, &error);
-      if (error)
-        {
-          printf ("WARNING: Unable to run the migrate favorites tools successfully: %s.\nThe output was:%s\n", error->message,output);
-          g_error_free (error);
-        }
-      g_free (output);
-      g_free (cmd);
-    }
-  g_free (latest_migration_update);
+  glib::String latest_migration_update(g_settings_get_string(
+                                           settings_, "favorite-migration"));
+  if (latest_migration_update.Str() < LATEST_SETTINGS_MIGRATION)
+  {
+    glib::Error error;
+    std::string cmd(PREFIXDIR);
+    cmd += "/lib/unity/migrate_favorites.py";
 
-  g_signal_connect (m_settings, "changed",
-                    G_CALLBACK (on_settings_updated), this);
+    glib::String output;
+
+    g_spawn_command_line_sync(cmd.c_str(),
+                              output.AsOutParam(),
+                              NULL,
+                              NULL,
+                              error.AsOutParam());
+    if (error)
+    {
+      // Do we have a defined logging subsystem?
+      std::cout << "WARNING: Unable to run the migrate favorites "
+                << "tools successfully: " << error.Message()
+                << ".\nThe output was:" << output.Value() << std::endl;
+    }
+  }
+
+  g_signal_connect(settings_, "changed", G_CALLBACK(on_settings_updated), this);
 
   Refresh ();
 }
 
-FavoriteStoreGSettings::~FavoriteStoreGSettings ()
-{
-  g_slist_foreach (m_favorites, (GFunc)g_free, NULL);
-  g_slist_free (m_favorites);
-  g_object_unref (m_settings);
-}
 
 /* If the desktop file exists, we *will* find it dang it
  * Returns null if we failed =(
@@ -100,13 +108,13 @@ char *exhaustive_desktopfile_lookup (char *desktop_file)
   char  *subpath;
   char **data_dirs = NULL;
   char **data;
-  
+
   env = g_getenv ("XDG_DATA_DIRS");
-  
+
   if (env)
   {
     data_dirs = g_strsplit (env, ":", 0);
-    
+
     for (data = data_dirs; *data; data++)
     {
       path = g_build_filename (*data, "applications", NULL);
@@ -116,57 +124,57 @@ char *exhaustive_desktopfile_lookup (char *desktop_file)
         g_free (path);
     }
   }
-  
+
   if (!g_list_find_custom (dirs, "/usr/share/applications", (GCompareFunc) g_strcmp0))
     dirs = g_list_prepend (dirs, g_strdup ("/usr/share/applications"));
-  
+
   if (!g_list_find_custom (dirs, "/usr/local/share/applications", (GCompareFunc) g_strcmp0))
     dirs = g_list_prepend (dirs, g_strdup ("/usr/local/share/applications"));
-  
+
   dirs = g_list_prepend (dirs, g_strdup (g_build_filename (g_get_home_dir (), ".share/applications", NULL)));
-  
+
   if (data_dirs)
     g_strfreev (data_dirs);
-  
+
   /* include subdirs */
   for (l = dirs; l; l = l->next)
   {
     path = (char *)l->data;
-    
+
     file = g_file_new_for_path (path);
-    
+
     if (!g_file_query_exists (file, NULL))
     {
       g_object_unref (file);
       continue;
     }
-    
+
     enumerator = g_file_enumerate_children (file,
                                             "standard::*",
                                             G_FILE_QUERY_INFO_NOFOLLOW_SYMLINKS,
                                             NULL,
                                             NULL);
-    
+
     if (!enumerator)
       continue;
-    
+
     info = g_file_enumerator_next_file (enumerator, NULL, NULL);
     for (; info; info = g_file_enumerator_next_file (enumerator, NULL, NULL))
     {
       if (g_file_info_get_file_type (info) != G_FILE_TYPE_DIRECTORY)
         continue;
-      
+
       subpath = g_build_filename (path, g_file_info_get_name (info), NULL);
       /* append for non-recursive recursion love */
       dirs = g_list_append (dirs, subpath);
-      
+
       g_object_unref (info);
     }
-    
+
     g_object_unref (enumerator);
     g_object_unref (file);
   }
-  
+
   /* dirs now contains a list if lookup directories */
   /* go through the dir list and stat to check it exists */
   path = NULL;
@@ -185,8 +193,7 @@ char *exhaustive_desktopfile_lookup (char *desktop_file)
   return path;
 }
 
-void
-FavoriteStoreGSettings::Refresh ()
+void FavoriteStoreGSettings::Refresh()
 {
   int     i = 0;
   gchar **favs;
@@ -195,7 +202,7 @@ FavoriteStoreGSettings::Refresh ()
   g_slist_free (m_favorites);
   m_favorites = NULL;
 
-  favs = g_settings_get_strv (m_settings, "favorites");
+  favs = g_settings_get_strv (settings_, "favorites");
 
   while (favs[i] != NULL)
     {
@@ -315,7 +322,7 @@ FavoriteStoreGSettings::AddFavorite (const char *desktop_path,
     }
 
   m_ignore_signals = true;
-  if (!g_settings_set_strv (m_settings, "favorites", favs))
+  if (!g_settings_set_strv (settings_, "favorites", favs))
     g_warning ("Unable to add a new favorite '%s' at position '%u'", desktop_path, position);
   m_ignore_signals = false;
 
@@ -369,7 +376,7 @@ FavoriteStoreGSettings::RemoveFavorite (const char *desktop_path)
     }
 
   m_ignore_signals = true;
-  if (!g_settings_set_strv (m_settings, "favorites", favs))
+  if (!g_settings_set_strv (settings_, "favorites", favs))
     g_warning ("Unable to remove favorite '%s'", desktop_path);
   m_ignore_signals = false;
 
@@ -424,7 +431,7 @@ FavoriteStoreGSettings::MoveFavorite (const char *desktop_path,
   favs[i] = NULL;
 
   m_ignore_signals = true;
-  if (!g_settings_set_strv (m_settings, "favorites", favs))
+  if (!g_settings_set_strv (settings_, "favorites", favs))
     g_warning ("Unable to add a new favorite '%s' at position '%u'", desktop_path, position);
   m_ignore_signals = false;
 
@@ -454,7 +461,7 @@ FavoriteStoreGSettings::SetFavorites (std::list<const char *> desktop_paths)
   }
   
   m_ignore_signals = true;
-  if (!g_settings_set_strv (m_settings, "favorites", favs))
+  if (!g_settings_set_strv (settings_, "favorites", favs))
     g_warning ("Unable to set favorites from list");
   m_ignore_signals = false;
   
