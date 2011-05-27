4a3732fd98cd03f6c98ce667b1e8f063b890bff2
Andrea Azzarone <azzaronea@gmail.com>
2011-05-27 20:57:49 +0200

    Try to fix bug #687956.
    The tooltip is deferred 1s after initial entry to the launcher, but
    once it's being shown for one icon, it is shown on others immediately as
    you move vertically from one to the other.
    
    (bzr r1200.2.1)

--- src/Launcher.cpp
+++ src/Launcher.cpp
@@ -3130,6 +3130,7 @@ void Launcher::RecvMouseLeave(int x, int y, unsigned long button_flags, unsigned
 {
   SetMousePosition (x, y);
   SetStateMouseOverLauncher (false);
+  LauncherIcon::SetSkipTooltipDelay (false);
 
   EventLogic ();
   EnsureAnimation ();
