04dc5f40a4de049f674c6e90152bcf869ecc5d97
Tim Penhey <tim.penhey@canonical.com>
2011-06-01 15:24:55 +1200

    test for active event emitting
    (bzr r1185.6.28)

--- tests/test_indicator_entry.cpp
+++ tests/test_indicator_entry.cpp
@@ -31,6 +31,52 @@ TEST(TestIndicatorEntry, TestConstruction) {
   // TODO: work out a nice test for the GdkPixbuf.
 }
 
+struct Counter : sigc::trackable
+{
+  Counter() : count(0) {}
+  void increment()
+    {
+      ++count;
+    }
+  int count;
+};
 
+template <typename T>
+struct ChangeRecorder : sigc::trackable
+{
+  void value_changed(T const& value)
+    {
+      changed_values.push_back(value);
+    }
+  typedef std::vector<T> ChangedValues;
+  ChangedValues changed_values;
+};
+
+
+TEST(TestIndicatorEntry, TestActiveEvents) {
+
+  indicator::Entry entry("id", "label", true, true,
+                         0, "some icon", false, true);
+
+  ChangeRecorder<bool> recorder;
+  Counter counter;
+  entry.updated.connect(sigc::mem_fun(counter, &Counter::increment));
+  entry.active_changed.connect(sigc::mem_fun(recorder, &ChangeRecorder<bool>::value_changed));
+
+  // Setting to the same value doesn't emit any events.
+  entry.set_active(false);
+  EXPECT_FALSE(entry.active());
+  EXPECT_EQ(counter.count, 0);
+  EXPECT_EQ(recorder.changed_values.size(), 0);
+
+  // Setting to a different value does emit the events.
+  entry.set_active(true);
+  EXPECT_TRUE(entry.active());
+  EXPECT_EQ(counter.count, 1);
+  ASSERT_EQ(recorder.changed_values.size(), 1);
+  EXPECT_TRUE(recorder.changed_values[0]);
+
+
+}
 
 }
