f0fbaf1e8a1af36c7a9a31d1a8dd4b643b025299
Tim Penhey <tim.penhey@canonical.com>
2011-06-02 13:25:21 +1200

    Move the implementation anonymous namespace to the bottom of the file and add (move) a method to make a pixbuf from the image type and image data.
    (bzr r1208.2.1)

--- src/PanelIndicatorObjectEntryView.cpp
+++ src/PanelIndicatorObjectEntryView.cpp
@@ -40,6 +40,7 @@ namespace unity {
 
 namespace {
 void draw_menu_bg(cairo_t *cr, int width, int height);
+GdkPixbuf* make_pixbuf(int image_type, std::string const& image_data);
 }
 
 
@@ -121,7 +122,6 @@ void PanelIndicatorObjectEntryView::Activate()
 // 3. Paint something
 void PanelIndicatorObjectEntryView::Refresh()
 {
-  GdkPixbuf            *pixbuf = proxy_->GetPixbuf ();
   PangoLayout          *layout = NULL;
   PangoFontDescription *desc = NULL;
   PangoAttrList        *attrs = NULL;
@@ -132,6 +132,9 @@ void PanelIndicatorObjectEntryView::Refresh()
   int                   dpi = 0;
 
   std::string label = proxy_->label();
+  glib::Object<GdkPixbuf> pixbuf = make_pixbuf(proxy_->image_type(),
+                                               proxy_->image_data());
+
 
   int  x = 0;
   int  y = 0;
@@ -294,8 +297,71 @@ void PanelIndicatorObjectEntryView::Refresh()
   NeedRedraw ();
 
   refreshed.emit(this);
-  if (pixbuf)
-    g_object_unref (pixbuf);
+}
+
+const gchar* PanelIndicatorObjectEntryView::GetName()
+{
+  if (proxy_->IsUnused())
+    return NULL;
+  else
+    return proxy_->id().c_str();
+}
+
+void PanelIndicatorObjectEntryView::AddProperties (GVariantBuilder *builder)
+{
+  variant::BuilderWrapper(builder)
+    .add(GetGeometry())
+    .add("label", proxy_->label())
+    .add("label_sensitive", proxy_->label_sensitive())
+    .add("label_visible", proxy_->label_visible())
+    .add("icon_sensitive", proxy_->image_sensitive())
+    .add("icon_visible", proxy_->image_visible())
+    .add("active", proxy_->active());
+}
+
+bool PanelIndicatorObjectEntryView::GetShowNow()
+{
+  return proxy_.get() ? proxy_->show_now() : false;
+}
+
+void PanelIndicatorObjectEntryView::GetGeometryForSync(GVariantBuilder* builder,
+                                                       const char* name)
+{
+  if (proxy_->IsUnused())
+    return;
+
+  nux::Geometry geo = GetAbsoluteGeometry();
+  g_variant_builder_add(builder, "(ssiiii)",
+                        name,
+                        proxy_->id().c_str(),
+                        geo.x,
+                        geo.y,
+                        geo.GetWidth(),
+                        geo.GetHeight());
+}
+
+bool PanelIndicatorObjectEntryView::IsEntryValid() const
+{
+  if (proxy_.get()) {
+    return proxy_->image_visible() || proxy_->label_visible();
+  }
+  return false;
+}
+
+bool PanelIndicatorObjectEntryView::IsSensitive() const
+{
+  if (proxy_.get()) {
+    return proxy_->image_sensitive() || proxy_->label_sensitive();
+  }
+  return false;
+}
+
+void PanelIndicatorObjectEntryView::OnFontChanged(GObject *gobject,
+                                                  GParamSpec *pspec,
+                                                  gpointer data)
+{
+  PanelIndicatorObjectEntryView *self = reinterpret_cast<PanelIndicatorObjectEntryView*>(data);
+  self->Refresh();
 }
 
 namespace {
@@ -382,71 +448,49 @@ void draw_menu_bg(cairo_t* cr, int width, int height)
   cairo_stroke (cr);
   cairo_pattern_destroy (pat);
 }
+
+GdkPixbuf* make_pixbuf(int image_type, std::string const& image_data)
+{
+  GdkPixbuf* ret = NULL;
+
+  if (image_type == GTK_IMAGE_PIXBUF)
+  {
+    gsize len = 0;
+    guchar* decoded = g_base64_decode(image_data.c_str(), &len);
+
+    GInputStream* stream = g_memory_input_stream_new_from_data(decoded,
+                                                               len, NULL);
+
+    ret = gdk_pixbuf_new_from_stream(stream, NULL, NULL);
+
+    g_free(decoded);
+    g_input_stream_close(stream, NULL, NULL);
+  }
+  else if (image_type == GTK_IMAGE_STOCK ||
+           image_type == GTK_IMAGE_ICON_NAME)
+  {
+    ret = gtk_icon_theme_load_icon(gtk_icon_theme_get_default(),
+                                   image_data.c_str(),
+                                   22,
+                                   (GtkIconLookupFlags)0,
+                                   NULL);
+  }
+  else if (image_type == GTK_IMAGE_GICON)
+  {
+    glib::Object<GIcon> icon(g_icon_new_for_string(image_data.c_str(), NULL));
+    GtkIconInfo* info = gtk_icon_theme_lookup_by_gicon(
+        gtk_icon_theme_get_default(), icon, 22, (GtkIconLookupFlags)0);
+    if (info)
+    {
+      ret = gtk_icon_info_load_icon(info, NULL);
+      gtk_icon_info_free(info);
+    }
+  }
+
+  return ret;
+}
+
 } // anon namespace
 
-const gchar* PanelIndicatorObjectEntryView::GetName()
-{
-  if (proxy_->IsUnused())
-    return NULL;
-  else
-    return proxy_->id().c_str();
-}
-
-void PanelIndicatorObjectEntryView::AddProperties (GVariantBuilder *builder)
-{
-  variant::BuilderWrapper(builder)
-    .add(GetGeometry())
-    .add("label", proxy_->label())
-    .add("label_sensitive", proxy_->label_sensitive())
-    .add("label_visible", proxy_->label_visible())
-    .add("icon_sensitive", proxy_->image_sensitive())
-    .add("icon_visible", proxy_->image_visible())
-    .add("active", proxy_->active());
-}
-
-bool PanelIndicatorObjectEntryView::GetShowNow()
-{
-  return proxy_.get() ? proxy_->show_now() : false;
-}
-
-void PanelIndicatorObjectEntryView::GetGeometryForSync(GVariantBuilder* builder,
-                                                       const char* name)
-{
-  if (proxy_->IsUnused())
-    return;
-
-  nux::Geometry geo = GetAbsoluteGeometry();
-  g_variant_builder_add(builder, "(ssiiii)",
-                        name,
-                        proxy_->id().c_str(),
-                        geo.x,
-                        geo.y,
-                        geo.GetWidth(),
-                        geo.GetHeight());
-}
-
-bool PanelIndicatorObjectEntryView::IsEntryValid() const
-{
-  if (proxy_.get()) {
-    return proxy_->image_visible() || proxy_->label_visible();
-  }
-  return false;
-}
-
-bool PanelIndicatorObjectEntryView::IsSensitive() const
-{
-  if (proxy_.get()) {
-    return proxy_->image_sensitive() || proxy_->label_sensitive();
-  }
-  return false;
-}
-
-void PanelIndicatorObjectEntryView::OnFontChanged(GObject *gobject,
-                                                  GParamSpec *pspec,
-                                                  gpointer data)
-{
-  PanelIndicatorObjectEntryView *self = reinterpret_cast<PanelIndicatorObjectEntryView*>(data);
-  self->Refresh();
-}
 
 } // namespace unity
