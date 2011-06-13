ae30ce8951cc48e5cc894d0cac3abd5246ec8e5d
Jay Taoko <jay.taoko@canonical.com>
2011-06-12 20:15:13 -0400

    * Nux event architecture: fixed most of the dash
    (bzr r1238.3.2)

--- src/PlacesSearchBar.cpp
+++ src/PlacesSearchBar.cpp
@@ -98,7 +98,6 @@ PlacesSearchBar::PlacesSearchBar (NUX_FILE_LINE_DECL)
   _layout->SetHorizontalExternalMargin (18);
 
   SetLayout (_layout);
-  SetCompositionLayout (_layout);
 
   _font_changed_id = g_signal_connect (gtk_settings_get_default (), "notify::gtk-font-name",
                                        G_CALLBACK (OnFontChanged), this);
