93775e41059628fc74b972f9db054d9209f5f07d
Neil Jagdish Patel <neil.patel@canonical.com>
2011-07-07 22:29:48 +0100

    Import Signal work as merging old trunk was breaking things
    (bzr r1270.2.1)

---
+++ UnityCore/GLibSignal-inl.h
@@ -0,0 +1,224 @@
+// -*- Mode: C++; indent-tabs-mode: nil; tab-width: 2 -*-
+/*
+* Copyright (C) 2011 Canonical Ltd
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Authored by: Neil Jagdish patel <neil.patel@canonical.com>
+*/
+
+#ifndef UNITY_GLIB_SIGNAL_INL_H
+#define UNITY_GLIB_SIGNAL_INL_H
+
+namespace unity {
+namespace glib {
+
+SignalBase::SignalBase()
+  : object_(0),
+    connection_id_(0)
+{}
+
+SignalBase::~SignalBase()
+{
+  Disconnect();
+}
+
+void SignalBase::Disconnect()
+{
+  if (G_IS_OBJECT(object_) && connection_id_)
+    g_signal_handler_disconnect(object_, connection_id_);
+  
+  object_ = 0;
+  connection_id_ = 0;
+}
+
+template <typename R>
+void Signal0<R>::Connect(GObject*           object,
+                         std::string const& signal_name,
+                         SignalCallback     callback)
+{
+  object_ = object;
+  callback_ = callback;
+  connection_id_ = g_signal_connect(object, signal_name.c_str (),
+                                    G_CALLBACK(Callback), this);
+}
+
+template <typename R>
+R Signal0<R>::Callback(GObject* object, Signal0* self)
+{
+  return self->callback_ ();
+}
+  
+template <typename R, typename T>
+void Signal1<R, T>::Connect(GObject*           object,
+                            std::string const& signal_name,
+                            SignalCallback     callback)
+{
+  object_ = object;
+  callback_ = callback;
+  connection_id_ = g_signal_connect(object, signal_name.c_str (),
+                                    G_CALLBACK (Callback), this);
+}
+
+template <typename R, typename T>
+R Signal1<R, T>::Callback(GObject* object, T data1, Signal1* self)
+{
+  return self->callback_ (data1);
+}
+
+template <typename R, typename T1, typename T2>
+void Signal2<R, T1, T2>::Connect(GObject*           object,
+                                 std::string const& signal_name,
+                                 SignalCallback     callback)
+{
+  object_ = object;
+  callback_ = callback;
+  connection_id_ = g_signal_connect(object, signal_name.c_str (),
+                                    G_CALLBACK (Callback), this);
+}
+
+template <typename R, typename T1, typename T2>
+R Signal2<R, T1, T2>::Callback(GObject* object,
+                               T1       data1,
+                               T2       data2,
+                               Signal2* self)
+{
+  return self->callback_ (data1, data2);
+}
+
+template <typename R, typename T1, typename T2, typename T3>
+void Signal3<R, T1, T2, T3>::Connect(GObject*           object,
+                                     std::string const& signal_name,
+                                     SignalCallback     callback)
+{
+  object_ = object;
+  callback_ = callback;
+  connection_id_ = g_signal_connect(object, signal_name.c_str (),
+                                    G_CALLBACK (Callback), this);
+}
+
+template <typename R, typename T1, typename T2, typename T3>
+R Signal3<R, T1, T2, T3>::Callback(GObject* object,
+                                   T1       data1,
+                                   T2       data2,
+                                   T3       data3,
+                                   Signal3* self)
+{
+  return self->callback_ (data1, data2, data3);
+}
+
+template <typename R, typename T1, typename T2, typename T3, typename T4>
+void Signal4<R, T1, T2, T3, T4>::Connect(GObject*           object,
+                                         std::string const& signal_name,
+                                         SignalCallback     callback)
+{
+  object_ = object;
+  callback_ = callback;
+  connection_id_ = g_signal_connect(object, signal_name.c_str (),
+                                    G_CALLBACK (Callback), this);
+}
+
+template <typename R, typename T1, typename T2, typename T3, typename T4>
+R Signal4<R, T1, T2, T3, T4>::Callback(GObject* object,
+                                       T1       data1,
+                                       T2       data2,
+                                       T3       data3,
+                                       T4       data4,
+                                       Signal4* self)
+{
+  return self->callback_ (data1, data2, data3, data4);
+}
+
+template <typename R, typename T1, typename T2,
+          typename T3, typename T4, typename T5 >
+void Signal5<R, T1, T2, T3, T4, T5>::Connect(GObject*           object,
+                                             std::string const& signal_name,
+                                             SignalCallback     callback)
+{
+  object_ = object;
+  callback_ = callback;
+  connection_id_ = g_signal_connect(object, signal_name.c_str (),
+                                    G_CALLBACK (Callback), this);
+}
+
+template <typename R, typename T1, typename T2,
+          typename T3, typename T4, typename T5>
+R Signal5<R, T1, T2, T3, T4, T5>::Callback(GObject* object,
+                                           T1       data1,
+                                           T2       data2,
+                                           T3       data3,
+                                           T4       data4,
+                                           T5       data5,
+                                           Signal5* self)
+{
+  return self->callback_ (data1, data2, data3, data4, data5);
+}
+
+template <typename R, typename T1, typename T2,
+          typename T3, typename T4, typename T5 , typename T6>
+void Signal6<R, T1, T2, T3, T4, T5, T6>::Connect(GObject*           object,
+                                                 std::string const& signal_name,
+                                                 SignalCallback     callback)
+{
+  object_ = object;
+  callback_ = callback;
+  connection_id_ = g_signal_connect(object, signal_name.c_str (),
+                                    G_CALLBACK (Callback), this);
+}
+
+template <typename R, typename T1, typename T2,
+          typename T3, typename T4, typename T5, typename T6>
+R Signal6<R, T1, T2, T3, T4, T5, T6>::Callback(GObject* object,
+                                               T1       data1,
+
+                                               T3       data3,
+                                               T4       data4,
+                                               T5       data5,
+                                               T6       data6,
+                                               Signal6* self)
+{
+  return self->callback_ (data1, data2, data3, data4, data5, data6);
+}
+
+template <typename R, typename T1, typename T2, typename T3,
+          typename T4, typename T5, typename T6 , typename T7>
+void Signal7<R, T1, T2, T3, T4, T5, T6, T7>::Connect(GObject*           object,
+                                                     std::string const& signal_name,
+                                                     SignalCallback     callback)
+{
+  object_ = object;
+  callback_ = callback;
+  connection_id_ = g_signal_connect(object, signal_name.c_str (),
+                                    G_CALLBACK (Callback), this);
+}
+
+template <typename R, typename T1, typename T2, typename T3,
+          typename T4, typename T5, typename T6, typename T7>
+R Signal7<R, T1, T2, T3, T4, T5, T6, T7>::Callback(GObject* object,
+                                                   T1       data1,
+                                                   T2       data2,
+                                                   T3       data3,
+                                                   T4       data4,
+                                                   T5       data5,
+                                                   T6       data6,
+                                                   T7       data7,
+                                                   Signal7* self)
+{
+  return self->callback_ (data1, data2, data3, data4, data5, data6, data7);
+}
+
+
+}
+}
+
+#endif
---
+++ UnityCore/GLibSignal.h
@@ -0,0 +1,303 @@
+// -*- Mode: C++; indent-tabs-mode: nil; tab-width: 2 -*-
+/*
+* Copyright (C) 2011 Canonical Ltd
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Authored by: Neil Jagdish Patel <neil.patel@canonical.com>
+*/
+
+#ifndef UNITY_GLIB_SIGNAL_H
+#define UNITY_GLIB_SIGNAL_H
+
+#include <string>
+#include <glib-object.h>
+#include <sigc++/sigc++.h>
+
+namespace unity {
+namespace glib {
+
+class SignalBase
+{
+public:
+  SignalBase();
+  virtual ~SignalBase();
+  
+  void Disconnect();
+
+protected:
+  GObject* object_;
+  guint32 connection_id_;
+};
+
+template <typename R>
+class Signal0 : public SignalBase
+{
+public:
+  typedef sigc::slot<R> SignalCallback;
+
+  Signal0()
+  {}
+  
+  void Connect(GObject*           object,
+               std::string const& signal_name,
+               SignalCallback     cb);
+private:
+  static R Callback(GObject* object, Signal0* self);
+private:
+  SignalCallback callback_;
+};
+
+template <typename R, typename T>
+class Signal1 : public SignalBase
+{
+public:
+  typedef sigc::slot<R, T> SignalCallback;
+
+  Signal1()
+  {}
+
+  void Connect(GObject*           object,
+               std::string const& signal_name,
+               SignalCallback     callback);
+private:
+  static R Callback(GObject* object, T data1, Signal1* self);
+private:
+  SignalCallback callback_;
+};
+
+template <typename R, typename T1, typename T2>
+class Signal2 : public SignalBase
+{
+public:
+  typedef sigc::slot<R, T1, T2> SignalCallback;
+
+  Signal2()
+  {}
+
+  void Connect(GObject*           object,
+               std::string const& signal_name,
+               SignalCallback     callback);
+private:
+  static R Callback(GObject* object,
+                    T1       data1,
+                    T2       data2,
+                    Signal2* self);
+private:
+  SignalCallback callback_;
+};
+
+template <typename R, typename T1, typename T2, typename T3>
+class Signal3 : public SignalBase
+{
+public:
+  typedef sigc::slot<R, T1, T2, T3> SignalCallback;
+
+  Signal3()
+  {}
+
+  void Connect(GObject*           object,
+               std::string const& signal_name,
+               SignalCallback     callback);
+private:
+  static R Callback(GObject* object,
+                    T1       data1,
+                    T2       data2,
+                    T3       data3,
+                    Signal3* self);
+private:
+  SignalCallback callback_;
+};
+
+template <typename R, typename T1, typename T2, typename T3, typename T4>
+class Signal4 : public SignalBase
+{
+public:
+  typedef sigc::slot<R, T1, T2, T3, T4> SignalCallback;
+
+  Signal4()
+  {}
+
+  void Connect(GObject*           object,
+               std::string const& signal_name,
+               SignalCallback     callback);
+private:
+  static R Callback(GObject* object,
+                    T1       data1,
+                    T2       data2,
+                    T3       data3,
+                    T4       data4,
+                    Signal4* self);
+private:
+  SignalCallback callback_;
+};
+
+template <typename R, typename T1, typename T2,
+          typename T3, typename T4, typename T5>
+class Signal5 : public SignalBase
+{
+public:
+  typedef sigc::slot<R, T1, T2, T3, T4, T5> SignalCallback;
+
+  Signal5()
+  {}
+
+  void Connect(GObject*           object,
+               std::string const& signal_name,
+               SignalCallback     callback);
+private:
+  static R Callback(GObject* object,
+                    T1       data1,
+                    T2       data2,
+                    T3       data3,
+                    T4       data4,
+                    T5       data5,
+                    Signal5* self);
+private:
+  SignalCallback callback_;
+};
+
+template <typename R, typename T1, typename T2,
+          typename T3, typename T4, typename T5, typename T6>
+class Signal6 : public SignalBase
+{
+public:
+  typedef sigc::slot<R, T1, T2, T3, T4, T5, T6> SignalCallback;
+
+  Signal6()
+  {}
+
+  void Connect(GObject*           object,
+               std::string const& signal_name,
+               SignalCallback     callback);
+private:
+  static R Callback(GObject* object,
+                    T1       data1,
+                    T2       data2,
+                    T3       data3,
+                    T4       data4,
+                    T5       data5,
+                    T6       data6,
+                    Signal6* self);
+private:
+  SignalCallback callback_;
+};
+
+template <typename R, typename T1, typename T2, typename T3,
+          typename T4, typename T5, typename T6, typename T7>
+class Signal7 : public SignalBase
+{
+public:
+  typedef sigc::slot<R, T1, T2, T3, T4, T5, T6, T7> SignalCallback;
+
+  Signal7()
+  {}
+
+  void Connect(GObject*           object,
+               std::string const& signal_name,
+               SignalCallback     callback);
+private:
+  static R Callback(GObject* object,
+                    T1       data1,
+                    T2       data2,
+                    T3       data3,
+                    T4       data4,
+                    T5       data5,
+                    T6       data6,
+                    T7       data7,
+                    Signal7* self);
+private:
+  SignalCallback callback_;
+};
+
+struct nil;
+
+template <typename R, typename T1 = nil, typename T2 = nil,
+                      typename T3 = nil, typename T4 = nil,
+                      typename T5 = nil, typename T6 = nil,
+                      typename T7 = nil>
+class Signal : public Signal1<R, T1, T2, T3, T4, T5, T6, T7>
+{
+public:
+  inline Signal() {}
+};
+
+template <typename R>
+class Signal<R, nil, nil, nil, nil, nil, nil, nil> : public Signal0<R>
+{
+public:
+  inline Signal() {}
+};
+
+template <typename R, typename T1>
+class Signal<R, T1, nil, nil, nil, nil, nil, nil> : public Signal1<R, T1>
+{
+public:
+  inline Signal() {}
+};
+
+template <typename R, typename T1, typename T2>
+class Signal<R, T1, T2, nil, nil, nil, nil, nil> : public Signal2<R, T1, T2>
+{
+public:
+  inline Signal() {}
+};
+
+template <typename R, typename T1, typename T2, typename T3>
+class Signal<R, T1, T2, T3, nil, nil, nil, nil> : public Signal3<R, T1, T2 ,T3>
+{
+public:
+  inline Signal() {}
+};
+
+template <typename R, typename T1, typename T2, typename T3, typename T4>
+class Signal<R, T1, T2, T3, T4, nil, nil, nil>
+  : public Signal4<R, T1, T2 ,T3, T4>
+{
+public:
+  inline Signal() {}
+};
+
+template <typename R, typename T1, typename T2, typename T3, typename T4,
+          typename T5>
+class Signal<R, T1, T2, T3, T4, T5, nil, nil>
+  : public Signal5<R, T1, T2 ,T3, T4, T5>
+{
+public:
+  inline Signal() {}
+};
+
+template <typename R, typename T1, typename T2, typename T3, typename T4,
+          typename T5, typename T6>
+class Signal<R, T1, T2, T3, T4, T5, T6, nil>
+  : public Signal6<R, T1, T2 ,T3, T4, T5, T6>
+{
+public:
+  inline Signal() {}
+};
+
+template <typename R, typename T1, typename T2, typename T3, typename T4,
+          typename T5, typename T6, typename T7>
+class Signal<R, T1, T2, T3, T4, T5, T6, T7>
+  : public Signal7<R, T1, T2 ,T3, T4, T5, T6, T7>
+{
+public:
+  inline Signal() {}
+};
+
+}
+}
+
+#include "GLibSignal-inl.h"
+
+#endif
