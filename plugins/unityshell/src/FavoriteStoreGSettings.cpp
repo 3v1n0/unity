a13677cb8d765f44510eea8490cf8fb2c1ab8b95
Tim Penhey <tim.penhey@canonical.com>
2011-07-29 15:02:15 +1200

    Use the new address of operators and streaming.
    (bzr r1315.2.2)

--- plugins/unityshell/src/FavoriteStoreGSettings.cpp
+++ plugins/unityshell/src/FavoriteStoreGSettings.cpp
@@ -86,16 +86,16 @@ void FavoriteStoreGSettings::Init()
     glib::String output;
 
     g_spawn_command_line_sync(cmd.c_str(),
-                              output.AsOutParam(),
+                              &output,
                               NULL,
                               NULL,
-                              error.AsOutParam());
+                              &error);
     if (error)
     {
       // Do we have a defined logging subsystem?
       std::cout << "WARNING: Unable to run the migrate favorites "
-                << "tools successfully: " << error.Message()
-                << ".\nThe output was:" << output.Value() << std::endl;
+                << "tools successfully: " << error
+                << ".\nThe output was:" << output << std::endl;
     }
   }
 
