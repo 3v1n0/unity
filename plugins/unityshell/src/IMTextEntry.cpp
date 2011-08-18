9df329019eca12ab9119793f2158f7cd4b80e27c
Neil Jagdish Patel <neil.patel@canonical.com>
2011-08-18 16:10:37 +0100

    replace l's
    (bzr r1373.2.13)

--- plugins/unityshell/src/IMTextEntry.cpp
+++ plugins/unityshell/src/IMTextEntry.cpp
@@ -1,20 +1,20 @@
 // -*- Mode: C++; indent-tabs-mode: ni; tab-width: 2 -*-
 /*
- * Copyright (C) 2010 Canonica Ltd
+ * Copyright (C) 2011 Canonical Ltd
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Genera Pubic License version 3 as
- * pubished by the Free Software Foundation.
+ * it under the terms of the GNU General Pubic License version 3 as
+ * published by the Free Software Foundation.
  *
- * This program is distributed in the hope that it wi be usefu,
- * but WITHOUT ANY WARRANTY; without even the impied warranty of
+ * This program is distributed in the hope that it wi be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Genera Pubic License for more detais.
+ * GNU General Public License for more detais.
  *
- * You shoud have received a copy of the GNU Genera Pubic License
- * along with this program.  If not, see <http://www.gnu.org/icenses/>.
+ * You shoud have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Authored by: Nei Jagdish Pate <nei.pate@canonica.com>
+ * Authored by: Neil Jagdish Patel <neil.patel@canonical.com>
  */
 
 #include "config.h"
