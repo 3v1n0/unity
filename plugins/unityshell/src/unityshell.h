24612622cb886250ecddadac54ba1b1c3c76f49d
Tim Penhey <tim.penhey@canonical.com>
2011-09-08 14:05:01 +1200

    Makes PlacesStyle a non-singleton, but warns you if you try to create more than one instance.
    This means that the unity screen can own the style, so it get
    nicely destroyed when the screen is destroyed.
    
    (bzr r1507.1.3)

--- plugins/unityshell/src/unityshell.h
+++ plugins/unityshell/src/unityshell.h
@@ -35,6 +35,7 @@
 #include "Launcher.h"
 #include "LauncherController.h"
 #include "PanelController.h"
+#include "PlacesStyle.h"
 #include "UScreen.h"
 #include "GestureEngine.h"
 #include "DebugDBusInterface.h"
@@ -117,6 +118,7 @@ private:
 #include <compiztoolbox/compiztoolbox.h>
 
 using unity::FontSettings;
+using unity::PlacesStyle;
 using namespace unity::switcher;
 using namespace unity::dash;
 using unity::UBusManager;
@@ -259,6 +261,7 @@ private:
   static void OnLauncherStartKeyNav(GVariant* data, void* value);
   static void OnLauncherEndKeyNav(GVariant* data, void* value);
 
+  PlacesStyle      places_style_;
   FontSettings            font_settings_;
   Launcher*               launcher;
   LauncherController*     controller;
