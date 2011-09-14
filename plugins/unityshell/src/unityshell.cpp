5af55a4a4beae32a28ab2a9b8840b6ef21f68372
Jason Smith <jason.smith@canonical.com>
2011-09-13 20:02:07 -0400

    fix issue where slow moving pointer would not trigger edge reveal
    (bzr r1540.1.1)

--- plugins/unityshell/src/unityshell.cpp
+++ plugins/unityshell/src/unityshell.cpp
@@ -956,7 +956,7 @@ gboolean UnityScreen::OnEdgeTriggerTimeout(gpointer data)
 {
   UnityScreen* self = reinterpret_cast<UnityScreen*>(data);
 
-  if (pointerX == 0)
+  if (pointerX <= 1)
   {
     if (abs(pointerY-self->_edge_pointerY) <= 5)
     {
@@ -999,7 +999,7 @@ bool UnityScreen::launcherRevealEdgeInitiate(CompAction* action,
   if (_edge_trigger_handle)
     g_source_remove(_edge_trigger_handle);
 
-  if (pointerX == 0)
+  if (pointerX <= 1)
   {
     _edge_pointerY = pointerY;
     _edge_trigger_handle = g_timeout_add(_edge_timeout,
