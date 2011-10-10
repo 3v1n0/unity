f21e5e23463737410d18ffd45ee5447d0239dd4c
Marco Trevisan (Treviño) <mail@3v1n0.net>
2011-10-11 00:48:43 +0200

    PanelMenuView: don't react to double-click when the dash is opened.
    Fixes bug #870844
    
    (bzr r1707.1.1)

--- plugins/unityshell/src/PanelMenuView.cpp
+++ plugins/unityshell/src/PanelMenuView.cpp
@@ -1327,7 +1327,7 @@ PanelMenuView::OnMaximizedGrabEnd(int x, int y, unsigned long, unsigned long)
 void
 PanelMenuView::OnMouseDoubleClicked(int x, int y, unsigned long button_flags, unsigned long)
 {
-  if (nux::GetEventButton(button_flags) != 1)
+  if (nux::GetEventButton(button_flags) != 1 || _places_showing)
     return;
 
   guint32 window_xid = GetMaximizedWindow();
