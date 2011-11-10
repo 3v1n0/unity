ae613442ed8d8748f88891deb0f0bb46e2fdf66d
Jay Taoko <jay.taoko@canonical.com>
2011-11-10 00:11:09 -0500

    * Fix broken rendering and text search entry: After typing some characters in the search entry, interaction with the mouse produces incorrect rendering.
    (bzr r1735.5.1)

--- plugins/unityshell/src/DashSearchBar.cpp
+++ plugins/unityshell/src/DashSearchBar.cpp
@@ -57,7 +57,7 @@ SearchBar::SearchBar(NUX_FILE_LINE_DECL)
   , can_refine_search(false)
   , live_search_timeout_(0)
 {
-  nux::BaseTexture* icon = dash::Style::Instance().GetSearchMagnifyIcon();
+  nux::BaseTexture *icon = dash::Style::Instance().GetSearchMagnifyIcon();
 
   bg_layer_ = new nux::ColorLayer(nux::Color(0xff595853), true);
 
@@ -232,12 +232,25 @@ void SearchBar::DrawContent(nux::GraphicsEngine& GfxContext, bool force_draw)
   GfxContext.PushClippingRectangle(geo);
 
   if (!IsFullRedraw())
+  {
     gPainter.PushLayer(GfxContext, bg_layer_->GetGeometry(), bg_layer_);
+  }
+  else
+  {
+    nux::GetPainter().PushPaintLayerStack();
+  }  
 
   layout_->ProcessDraw(GfxContext, force_draw);
 
   if (!IsFullRedraw())
+  {
     gPainter.PopBackground();
+  }
+  else
+  {
+    nux::GetPainter().PopPaintLayerStack();
+  }
+
   GfxContext.PopClippingRectangle();
 }
 
