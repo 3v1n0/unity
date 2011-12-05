50b28f884fc964c014afdfe6d52f3bb65154ebb5
Andrea Azzarone <azzaronea@gmail.com>
2011-12-05 19:12:01 +0100

    Ops...
    (bzr r1739.4.4)

--- plugins/unityshell/src/LensBar.cpp
+++ plugins/unityshell/src/LensBar.cpp
@@ -91,12 +91,10 @@ void LensBar::AddLens(Lens::Ptr& lens)
 
 void LensBar::Activate(std::string id)
 {
-  std::cout << "Activate " << id << std::endl;
   for (auto icon: icons_)
   {
     if (icon->id == id)
     {
-      std::cout << "SetActive " << id << std::endl;
       SetActive(icon);
       break;
     }
