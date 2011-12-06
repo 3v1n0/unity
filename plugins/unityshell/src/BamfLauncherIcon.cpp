66993280f190bddce4c14cf349150ec07609633b
Marco Trevisan (Treviño) <mail@3v1n0.net>
2011-12-06 05:07:03 +0100

    BamfLauncherIcon: don't allow clients to use markup on quicklists.
    (bzr r1739.5.4)

--- plugins/unityshell/src/BamfLauncherIcon.cpp
+++ plugins/unityshell/src/BamfLauncherIcon.cpp
@@ -1003,6 +1003,7 @@ std::list<DbusmenuMenuitem*> BamfLauncherIcon::GetMenus()
       if (dbusmenu_menuitem_property_get_bool(item, DBUSMENU_MENUITEM_PROP_VISIBLE))
       {
         first_separator_needed = true;
+        dbusmenu_menuitem_property_set_bool(item, "unity-use-markup", false);
 
         result.push_back(item);
       }
