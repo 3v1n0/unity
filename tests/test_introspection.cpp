37a12fff12c220a88bedca02822a8e4ecb3aaf01
Thomi Richards <thomir@gmail.com>
2011-12-09 15:26:55 +1300

    Simple relative queries now work.
    (bzr r1733.6.19)

--- tests/test_introspection.cpp
+++ tests/test_introspection.cpp
@@ -123,3 +123,21 @@ TEST_F(TestIntrospection, TestMalformedRelativeQueries)
     EXPECT_STREQ("Foo", p->GetName().c_str()); 
   }
 }
+
+TEST_F(TestIntrospection, TestSimpleRelativeQueries)
+{
+  std::list<Introspectable*> results;
+  std::string query = "//Unity";
+
+  results = FindQueryStartPoints(query, root_.get());
+  ASSERT_EQ(1, results.size());
+  EXPECT_STREQ("Unity", results.front()->GetName().c_str()); 
+
+  query = "//Foo";
+  results = FindQueryStartPoints(query, root_.get());
+  ASSERT_EQ(3, results.size());
+  for(auto p : results)
+  {
+    EXPECT_STREQ("Foo", p->GetName().c_str()); 
+  }
+}
