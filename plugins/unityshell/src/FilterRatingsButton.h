b134f9998d454a1b0d59511fe5f091d97005eec3
Andrea Azzarone <azzaronea@gmail.com>
2011-12-15 02:52:21 +0100

    Add FilterAllButton class too.
    (bzr r1776.3.4)

--- plugins/unityshell/src/FilterRatingsButton.h
+++ plugins/unityshell/src/FilterRatingsButton.h
@@ -52,7 +52,6 @@ protected:
 
   void InitTheme();
 
-  //void RecvMouseDown(int x, int y, unsigned long button_flags, unsigned long key_flags);
   void RecvMouseUp(int x, int y, unsigned long button_flags, unsigned long key_flags);
   void RecvMouseDrag(int x, int y, int dx, int dy, unsigned long button_flags, unsigned long key_flags);
   void OnRatingsChanged(int rating);
