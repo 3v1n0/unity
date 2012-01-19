6e81822b4e2c609ed649ab256d2fc80552804706
Thomi Richards <thomir@gmail.com>
2012-01-19 17:42:01 +1300

    duit now uses pydot only.
    (bzr r1845.4.2)

--- tests/autopilot/duit.py
+++ tests/autopilot/duit.py
@@ -4,14 +4,6 @@
 # Script to generate a nice PNG file of the currently running unity introspection tree.
 import dbus
 from autopilot.emulators.unity import Unity
-from StringIO import StringIO
-
-try:
-    import gvgen
-except ImportError:
-    print "Error: the 'gvgen' module is required to run this script."
-    print "Try installing the 'python-gvgen' package."
-    exit(1)
 
 try:
     import pydot
@@ -24,10 +16,16 @@ def string_rep(dbus_type):
         return repr(bool(dbus_type))
     if type(dbus_type) == dbus.String:
         return str(dbus_type)
-    if type(dbus_type) in (dbus.Int16, dbus.Int32, dbus.Int64):
+    if type(dbus_type) in (dbus.Int16, dbus.UInt16, dbus.Int32, dbus.UInt32, dbus.Int64, dbus.UInt64):
         return repr(int(dbus_type))
+    if type(dbus_type) == dbus.Double:
+        return repr(float(dbus_type))
+    if type(dbus_type) == dbus.Array:
+        return ', '.join([string_rep(i) for i in dbus_type])
     else:
         return repr(dbus_type)
+def escape(s):
+    return pydot.quote_if_necessary(s)
 
 def traverse_graph(state, parent, graph):
     for key in state.keys():
@@ -35,18 +33,23 @@ def traverse_graph(state, parent, graph):
         if key == 'Children':
             # Add all array nodes as children of this node.
             for child_name, child_state in state[key]:
-                traverse_graph(child_state, graph.newItem(child_name, parent), graph)
+                child = pydot.Node(child_name)
+                graph.add_node(child)
+                graph.add_edge(pydot.Edge(parent, child))
+                traverse_graph(child_state, child, graph)
         else:
-            graph.propertyAppend(parent, key, string_rep(state[key]))
+            pass
+            #graph.propertyAppend(parent, escape(key), string_rep(state[key]))
 
 if __name__ == '__main__':
     u = Unity()
     introspection_tree = u.get_state()
-    graph = gvgen.GvGen()
-    gnode_unity = graph.newItem("Unity")
+    graph = pydot.Graph()
+    graph.set_simplify(False)
+    graph.set_node_defaults(shape='record')
+    gnode_unity = pydot.Node("Unity")
     traverse_graph(introspection_tree[0], gnode_unity, graph)
 
-    buff = StringIO()
-    graph.dot(buff)
-    g = pydot.graph_from_dot_data(buff.getvalue())
-    import pdb; pdb.set_trace()
+    #buff = StringIO()
+    #graph.dot(buff)
+    open('out.dot', 'w').write(graph.to_string())
