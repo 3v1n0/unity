0654f7a2090f1566836de50d4396d432248c3b6a
Mikkel Kamstrup Erlandsen <mikkel.kamstrup@gmail.com>
2012-01-20 12:23:25 +0100

    More tests for HomeLens
    (bzr r1825.2.42)

--- tests/test_home_lens.cpp
+++ tests/test_home_lens.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <glib-object.h>
+#include <dee.h>
 
 #include <UnityCore/GLibWrapper.h>
 #include <UnityCore/HomeLens.h>
@@ -20,9 +21,24 @@ TEST(TestHomeLens, TestConstruction)
 
   EXPECT_EQ(home_lens_.id(), "home.lens");
   EXPECT_EQ(home_lens_.connected, false);
-  EXPECT_EQ(home_lens_.name(), "name");
-  EXPECT_EQ(home_lens_.description(), "description");
-  EXPECT_EQ(home_lens_.search_hint(), "searchhint");
+  EXPECT_EQ(home_lens_.search_in_global, false);
+  EXPECT_EQ(home_lens_.name, "name");
+  EXPECT_EQ(home_lens_.description, "description");
+  EXPECT_EQ(home_lens_.search_hint, "searchhint");
+}
+
+TEST(TestHomeLens, TestInitiallyEmpty)
+{
+  HomeLens home_lens_("name", "description", "searchhint");
+  DeeModel* results = home_lens_.results()->model();
+  DeeModel* categories = home_lens_.categories()->model();;
+  DeeModel* filters = home_lens_.filters()->model();;
+
+  EXPECT_EQ(dee_model_get_n_rows(results), 0);
+  EXPECT_EQ(dee_model_get_n_rows(categories), 0);
+  EXPECT_EQ(dee_model_get_n_rows(filters), 0);
+
+  EXPECT_EQ(home_lens_.count(), 0);
 }
 
 }
