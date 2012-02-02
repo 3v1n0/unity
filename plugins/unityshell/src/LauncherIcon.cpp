2710401bc313b2d92dcb234c80cfa7f4fb52f330
Marco Trevisan (Treviño) <mail@3v1n0.net>
2012-02-03 00:37:21 +0100

    LauncherIcon: again, move the quicklist 1 px more to the left
    (bzr r1885.4.28)

--- plugins/unityshell/src/LauncherIcon.cpp
+++ plugins/unityshell/src/LauncherIcon.cpp
@@ -647,7 +647,7 @@ LauncherIcon::SetCenter(nux::Point3 center, int monitor, nux::Geometry geo)
   if (monitor == _last_monitor)
   {
     int tip_x, tip_y;
-    tip_x = geo.x + geo.width - 3 * geo.width / 48;
+    tip_x = geo.x + geo.width - 4 * geo.width / 48;
     tip_y = geo.y + _center[monitor].y;
 
     if (_quicklist->IsVisible())
