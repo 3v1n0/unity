df3c2e4187464a8030c65d5409f82848e9b3192d
Andrea Azzarone <azzaronea@gmail.com>
2012-02-02 20:10:04 +0100

    Adds UnityShellPrivate. Makes CreateActionSting testable. Tests it.
    (bzr r1826.4.3)

---
+++ plugins/unityshell/src/UnityshellPrivate.cpp
@@ -0,0 +1,44 @@
+// -*- Mode: C++; indent-tabs-mode: nil; tab-width: 2 -*-
+/*
+ * Copyright (C) 2012 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Andrea Azzarone <azzaronea@gmail.com>
+ */
+
+#include "UnityshellPrivate.h"
+
+namespace unity
+{
+namespace impl
+{
+
+std::string CreateActionString(std::string const& modifiers,
+                               std::string const& shortcut,
+                               ActionModifiers flag)
+{
+  std::string ret(modifiers);
+
+  if (flag == ActionModifiers::USE_SHIFT)
+    ret += "<Shift>";
+  else if (flag == ActionModifiers::USE_NUMPAD)
+    ret += "KP_";
+
+  ret += shortcut;
+
+  return ret;
+}
+
+} // namespace impl
+} // namespace unity
---
+++ plugins/unityshell/src/UnityshellPrivate.h
@@ -0,0 +1,45 @@
+// -*- Mode: C++; indent-tabs-mode: nil; tab-width: 2 -*-
+/*
+ * Copyright (C) 2012 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Andrea Azzarone <azzaronea@gmail.com>
+ */
+
+#ifndef UNITYSHELL_UNITYSHELL_PRIVATE_H
+#define UNITYSHELL_UNITYSHELL_PRIVATE_H
+
+#include <string>
+
+namespace unity
+{
+namespace impl
+{
+
+enum class ActionModifiers
+{
+  NONE = 0,
+  
+  USE_NUMPAD,
+  USE_SHIFT
+};
+
+std::string CreateActionString(std::string const& modifiers, 
+                               std::string const& shortcut,
+                               ActionModifiers flag = ActionModifiers::NONE);
+
+} // namespace impl
+} // namespace unity
+
+#endif // UNITYSHELL_UNITYSHELL_PRIVATE_H
