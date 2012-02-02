6900d8c37a882f444cf2d1e81f72684a9f680e3d
Marco Trevisan (Treviño) <mail@3v1n0.net>
2012-02-02 22:58:36 +0100

    CairoBaseWindow: added new class to create nux base windows with cairo elements
    (bzr r1885.5.1)

---
+++ plugins/unityshell/src/CairoBaseWindow.cpp
@@ -0,0 +1,151 @@
+// -*- Mode: C++; indent-tabs-mode: nil; tab-width: 2 -*-
+/*
+ * Copyright (C) 2012 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Marco Trevisan <3v1n0@ubuntu.com>
+ */
+
+
+#include <Nux/Nux.h>
+
+#include "CairoTexture.h"
+#include "CairoBaseWindow.h"
+
+namespace unity
+{
+namespace
+{
+  const int ANCHOR_WIDTH = 14;
+  const int ANCHOR_HEIGHT = 18;
+  const int CORNER_RADIUS = 4;
+  const int PADDING = 15;
+  const int TEXT_PADDING = 8;
+  const int MINIMUM_TEXT_WIDTH = 100;
+  const int TOP_SIZE = 0;
+}
+
+NUX_IMPLEMENT_OBJECT_TYPE(CairoBaseWindow);
+CairoBaseWindow::CairoBaseWindow() :
+  _use_blurred_background(false),
+  _compute_blur_bkg(false)
+{
+  SetWindowSizeMatchLayout(true);
+}
+
+void CairoBaseWindow::Draw(nux::GraphicsEngine& gfxContext, bool forceDraw)
+{
+  nux::Geometry base(GetGeometry());
+
+  // Get the background and apply some blur
+  if (_use_blurred_background && _compute_blur_bkg)
+  {
+    auto current_fbo = nux::GetGraphicsDisplay()->GetGpuDevice()->GetCurrentFrameBufferObject();
+    nux::GetGraphicsDisplay()->GetGpuDevice()->DeactivateFrameBuffer();
+
+    gfxContext.SetViewport(0, 0, gfxContext.GetWindowWidth(), gfxContext.GetWindowHeight());
+    gfxContext.SetScissor(0, 0, gfxContext.GetWindowWidth(), gfxContext.GetWindowHeight());
+    gfxContext.GetRenderStates().EnableScissor(false);
+
+    nux::ObjectPtr <nux::IOpenGLBaseTexture> bkg_texture = gfxContext.CreateTextureFromBackBuffer(base.x, base.y, base.width, base.height);
+
+    nux::TexCoordXForm texxform_bkg;
+    _bg_blur_texture = gfxContext.QRP_GetBlurTexture(0, 0, base.width, base.height, bkg_texture, texxform_bkg, nux::color::White, 1.0f, 3);
+
+    if (current_fbo.IsValid())
+    {
+      current_fbo->Activate(true);
+      gfxContext.Push2DWindow(current_fbo->GetWidth(), current_fbo->GetHeight());
+    }
+    else
+    {
+      gfxContext.SetViewport(0, 0, gfxContext.GetWindowWidth(), gfxContext.GetWindowHeight());
+      gfxContext.Push2DWindow(gfxContext.GetWindowWidth(), gfxContext.GetWindowHeight());
+      gfxContext.ApplyClippingRectangle();
+    }
+    _compute_blur_bkg = false;
+  }
+
+  // the elements position inside the window are referenced to top-left window
+  // corner. So bring base to (0, 0).
+  base.SetX(0);
+  base.SetY(0);
+  gfxContext.PushClippingRectangle(base);
+
+  nux::TexCoordXForm texxform_bg;
+  texxform_bg.SetWrap(nux::TEXWRAP_CLAMP, nux::TEXWRAP_CLAMP);
+  texxform_bg.SetTexCoordType(nux::TexCoordXForm::OFFSET_COORD);
+
+  nux::TexCoordXForm texxform_mask;
+  texxform_mask.SetWrap(nux::TEXWRAP_CLAMP, nux::TEXWRAP_CLAMP);
+  texxform_mask.SetTexCoordType(nux::TexCoordXForm::OFFSET_COORD);
+
+  if (_bg_blur_texture.IsValid() && _texture_mask.IsValid())
+  {
+    nux::TexCoordXForm texxform_blur_bkg;
+
+    gfxContext.QRP_2TexMod(
+      base.x,
+      base.y,
+      base.width,
+      base.height,
+      _bg_blur_texture,
+      texxform_blur_bkg,
+      nux::color::White,
+      _texture_mask->GetDeviceTexture(),
+      texxform_mask,
+      nux::color::White);
+  }
+
+  if (_texture_bg.IsValid() && _texture_mask.IsValid())
+  {
+    nux::GetWindowThread()->GetGraphicsEngine().GetRenderStates().SetBlend(true);
+    nux::GetWindowThread()->GetGraphicsEngine().GetRenderStates().SetPremultipliedBlend(nux::SRC_OVER);
+
+    gfxContext.QRP_2TexMod(base.x,
+                           base.y,
+                           base.width,
+                           base.height,
+                           _texture_bg->GetDeviceTexture(),
+                           texxform_bg,
+                           nux::color::White,
+                           _texture_mask->GetDeviceTexture(),
+                           texxform_mask,
+                           nux::color::White);
+  }
+
+  if (_texture_outline.IsValid())
+  {
+    nux::TexCoordXForm texxform;
+    texxform.SetWrap(nux::TEXWRAP_CLAMP, nux::TEXWRAP_CLAMP);
+    texxform.SetTexCoordType(nux::TexCoordXForm::OFFSET_COORD);
+
+    nux::GetWindowThread()->GetGraphicsDisplay().GetGraphicsEngine()->GetRenderStates().SetBlend(true);
+    nux::GetWindowThread()->GetGraphicsDisplay().GetGraphicsEngine()->GetRenderStates().SetPremultipliedBlend(nux::SRC_OVER);
+    
+    gfxContext.QRP_1Tex(base.x,
+                        base.y,
+                        base.width,
+                        base.height,
+                        _texture_outline->GetDeviceTexture(),
+                        texxform,
+                        nux::color::White);
+  }
+
+  nux::GetWindowThread()->GetGraphicsDisplay().GetGraphicsEngine()->GetRenderStates().SetBlend(false);
+
+  gfxContext.PopClippingRectangle();
+}
+
+} // namespace nux
