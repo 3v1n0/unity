3b262b5b905dee4032cdbb5b262cab1711b08df5
Marco Trevisan (Treviño) <mail@3v1n0.net>
2012-02-11 19:19:20 +0100

    UnityWindow: don't draw the panel shadow over a window if the integrated menus are enabled.
    (bzr r1858.9.101)

--- plugins/unityshell/src/unityshell.cpp
+++ plugins/unityshell/src/unityshell.cpp
@@ -1983,14 +1983,15 @@ bool UnityWindow::glDraw(const GLMatrix& matrix,
   }
 
   Window active_window = screen->activeWindow();
-  if (window->id() == active_window && window->type() != CompWindowTypeDesktopMask)
+  bool integrated_menus = panel::Style::Instance().integrated_menus;
+  if (window->id() == active_window && window->type() != CompWindowTypeDesktopMask && !integrated_menus)
   {
     uScreen->paintPanelShadow(matrix);
   }
 
   bool ret = gWindow->glDraw(matrix, attrib, region, mask);
 
-  if (active_window == 0 || active_window == window->id() && window->type() == CompWindowTypeDesktopMask)
+  if (active_window == 0 || (active_window == window->id() && window->type() == CompWindowTypeDesktopMask))
   {
     uScreen->paintPanelShadow(matrix);
   }
