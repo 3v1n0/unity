d521a6201ca108f3373488420d244b5c3ccfa0ee
Thomi Richards <thomi.richards@canonical.com>
2012-02-21 11:12:11 +1300

    Now activate ibus after loading dash.
    (bzr r1975.3.7)

--- tests/autopilot/autopilot/tests/test_ibus.py
+++ tests/autopilot/autopilot/tests/test_ibus.py
@@ -24,45 +24,61 @@ class IBusTests(AutopilotTestCase):
         self.dash = Dash()
 
     def tearDown(self):
-        self.kb.press_and_release('Ctrl+Space')
         set_active_engines(self.old_engines)
         super(IBusTests, self).tearDown()
+
+    def activate_ibus(self):
+        # it would be nice to be able to tell if it's currently active or not.
+        self.kb.press_and_release('Ctrl+Space')
+
+    def deactivate_ibus(self):
+        # it would be nice to be able to tell if it's currently active or not.
+        self.kb.press_and_release('Ctrl+Space')
 
     def test_simple_dash(self):
         """Entering 'abc1' in the dash with ibus enabled should result in u'\u963f\u5e03\u4ece'."""
         self.dash.ensure_visible()
+        self.activate_ibus()
         self.kb.type('abc1')
-        sleep(1)
         dash_search_string = self.dash.get_search_string()
+        self.deactivate_ibus()
         self.dash.ensure_hidden()
-        sleep(1)
+
         self.assertEqual(u'\u963f\u5e03\u4ece', dash_search_string)
 
     def test_photo(self):
         """Enter 照片 (Photo) to dash and check the search result"""
         self.dash.ensure_visible()
+        self.activate_ibus()
         self.kb.type("zhaopian ")
+        self.deactivate_ibus()
         self.assertEqual(self.dash.get_search_string(), u'照片')
         self.dash.ensure_hidden()
 
     def test_internet(self):
         """Enter 互联网 (internet) to dash and check the search result"""
         self.dash.ensure_visible()
+        self.activate_ibus()
         self.kb.type("hulianwang ")
+        self.deactivate_ibus()
         self.assertEqual(self.dash.get_search_string(), u'互联网')
         self.dash.ensure_hidden()
 
     def test_disk(self):
         """Enter 磁盘 (disk) to dash and check the search result"""
         self.dash.ensure_visible()
+        self.activate_ibus()
         self.kb.type("cipan ")
+        self.deactivate_ibus()
         self.assertEqual(self.dash.get_search_string(), u'磁盘')
         self.dash.ensure_hidden()
 
     def test_disk_management(self):
         """Enter 磁盘管理 (disk management) to dash and check the search result"""
         self.dash.ensure_visible()
+        self.activate_ibus()
         self.kb.type(u"cipan guanli ")
+        self.deactivate_ibus()
         self.assertEqual(self.dash.get_search_string(), u'磁盘管理')
         self.dash.ensure_hidden()
 
