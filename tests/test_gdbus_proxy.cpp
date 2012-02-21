cf4f14f56af98b183f4505607a63090acbdf5ba9
Gord Allott <gord.allott@canonical.com>
2012-02-21 16:13:50 +0000

    fix up the tests so they pass
    (bzr r1997.2.2)

--- tests/test_gdbus_proxy.cpp
+++ tests/test_gdbus_proxy.cpp
@@ -9,29 +9,34 @@ using namespace unity;
 namespace
 {
 
-GMainLoop* loop_ = g_main_loop_new(NULL, FALSE);
-glib::DBusProxy proxy("com.canonical.Unity.Test", 
-                      "/com/canonical/gdbus_wrapper", 
-                      "com.canonical.gdbus_wrapper");
+GMainLoop* loop_ = NULL;
+glib::DBusProxy* proxy = NULL;
 
 class TestGDBusProxy: public ::testing::Test
 {
 public:
   TestGDBusProxy()
     : connected_result(false)
+    , got_signal_return(false)
+    , got_result_return(false)
   {
   }
   bool connected_result;
+  bool got_signal_return;
+  bool got_result_return;
 };
 
 TEST_F(TestGDBusProxy, TestConstruction)
 {
-  
+  loop_ = g_main_loop_new(NULL, FALSE);
+  proxy = new glib::DBusProxy("com.canonical.Unity.Test", 
+                              "/com/canonical/gdbus_wrapper", 
+                              "com.canonical.gdbus_wrapper");
   // performs a check on the proxy, if the proxy is connected, report a sucess
   auto timeout_check = [] (gpointer data) -> gboolean
   {
     TestGDBusProxy* self = static_cast<TestGDBusProxy*>(data);
-    if (proxy.IsConnected())
+    if (proxy->IsConnected())
     {
       self->connected_result = true;
       g_main_loop_quit(loop_);
@@ -65,9 +70,11 @@ TEST_F(TestGDBusProxy, TestConstruction)
 
 TEST_F(TestGDBusProxy, TestMethodReturn)
 {
-  glib::String expected_return((gchar *)"TestStringTestString☻☻☻"); // cast to get gcc to shut up
-  gchar* returned_result = (gchar*)"Not equal"; // cast to get gcc to shut up
-  gchar* returned_signal = (gchar*)"Not equal"; // cast to get gcc to shut up
+  // Our service is setup so that if you call the TestMethod method, it will emit the TestSignal method
+  // with whatever string you pass in
+  gchar* expected_return = (gchar *)"TestStringTestString☻☻☻"; // cast to get gcc to shut up
+  gchar* returned_result = g_strdup("Not equal"); 
+  gchar* returned_signal = g_strdup("Not equal"); 
 
   GVariant* param_value = g_variant_new_string(expected_return);
   GVariant* parameters = g_variant_new_tuple(&param_value, 1);
@@ -76,8 +83,13 @@ TEST_F(TestGDBusProxy, TestMethodReturn)
   {
     if (variant != nullptr)
     {
-      returned_signal = (gchar*)g_variant_get_string(g_variant_get_child_value(variant, 0), NULL);
+      g_free(returned_signal);
+      returned_signal = g_strdup(g_variant_get_string(g_variant_get_child_value(variant, 0), NULL));
     }
+
+    got_signal_return = true;
+    if (got_signal_return && got_result_return)
+      g_main_loop_quit(loop_);
   };
 
   // method callback
@@ -85,8 +97,13 @@ TEST_F(TestGDBusProxy, TestMethodReturn)
   {
     if (variant != nullptr)
     {
-      returned_result = (gchar*)g_variant_get_string(g_variant_get_child_value(variant, 0), NULL);
+      g_free(returned_result);
+      returned_result = g_strdup(g_variant_get_string(g_variant_get_child_value(variant, 0), NULL));
     }
+
+    got_result_return = true;
+    if (got_signal_return && got_result_return)
+      g_main_loop_quit(loop_);
   };
   
   auto timeout_bailout = [] (gpointer data) -> gboolean // bail out after 10 seconds
@@ -97,9 +114,9 @@ TEST_F(TestGDBusProxy, TestMethodReturn)
    
   g_timeout_add_seconds(10, timeout_bailout, this);
 
-  EXPECT_EQ(proxy.IsConnected(), true); // fail if we are not connected
-  proxy.Connect("TestSignal", signal_connection);
-  proxy.Call("TestMethod", parameters, method_connection); 
+  EXPECT_EQ(proxy->IsConnected(), true); // fail if we are not connected
+  proxy->Connect("TestSignal", signal_connection);
+  proxy->Call("TestMethod", parameters, method_connection); 
 
  
   // next check we get 30 entries from this specific known callback
@@ -107,6 +124,9 @@ TEST_F(TestGDBusProxy, TestMethodReturn)
 
   EXPECT_EQ(g_strcmp0(expected_return, returned_result), 0);
   EXPECT_EQ(g_strcmp0(expected_return, returned_signal), 0);
+
+  g_free(returned_result);
+  g_free(returned_signal);
 }
 
 
