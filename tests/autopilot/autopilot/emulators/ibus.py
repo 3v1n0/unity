ad3b18e97327e700382ddeb9c53b5dca37cb4a44
Thomi Richards <thomi.richards@canonical.com>
2012-02-23 07:27:03 +1300

    Wait a bit after restarting the ibus bus.
    (bzr r1975.3.18)

--- tests/autopilot/autopilot/emulators/ibus.py
+++ tests/autopilot/autopilot/emulators/ibus.py
@@ -73,6 +73,7 @@ def set_active_engines(engine_list):
     # see bug report here:
     # http://code.google.com/p/ibus/issues/detail?id=1418&thanks=1418&ts=1329885137
     bus.exit(restart=True)
+    sleep(1)
     return old_engines
 
 def set_global_input_engine(engine_name):
