1b2b461fb9f9a4e0884157a05c5107c872efc7bd
Thomi Richards <thomi.richards@canonical.com>
2012-02-27 17:43:38 +1300

    Failing test: only setting instances with type=Key should be allowed.
    (bzr r2025.1.3)

--- tests/autopilot/tests/test_compiz_key_translate.py
+++ tests/autopilot/tests/test_compiz_key_translate.py
@@ -8,8 +8,11 @@
 
 from testtools import TestCase
 from testtools.matchers import raises
+from compizconfig import Context, Plugin, Setting
 
 from autopilot.utilities import get_keystroke_string_from_compiz_setting
+
+_ctx = Context()
 
 class CompizConfigKeyTranslateTests(TestCase):
     """Tests that the compizconfig keycode translation routes work as advertised."""
@@ -17,3 +20,16 @@ class CompizConfigKeyTranslateTests(TestCase):
     def test_requires_setting_instance(self):
         """Function must raise TypeError unless given an instance of compizconfig.Setting."""
         self.assertThat(lambda: get_keystroke_string_from_compiz_setting(True), raises(TypeError))
+
+    def test_requires_setting_key_type(self):
+        """Passed in settings object must be of type 'Key', otherwise ValueError is raised."""
+        plugin = Plugin(_ctx, "unityshell")
+
+        invalid_settings = ('launcher_hide_mode',
+            'alt_tab_timeout',
+            'background_color',
+            'panel_opacity',
+            'icon_size')
+        for setting_name in invalid_settings:
+            setting = Setting(plugin, setting_name)
+            self.assertThat(lambda: get_keystroke_string_from_compiz_setting(setting), raises(ValueError))
