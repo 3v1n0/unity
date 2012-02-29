5c07f2845bde4ca3634442d2d79f932a60de529b
Marco Trevisan (Treviño) <mail@3v1n0.net>
2012-02-29 02:34:15 +0100

    Added autopilot test for switcher mouse-wheel support.
    (bzr r2021.2.5)

--- tests/autopilot/autopilot/emulators/unity/switcher.py
+++ tests/autopilot/autopilot/emulators/unity/switcher.py
@@ -12,7 +12,7 @@ from time import sleep
 
 from autopilot.keybindings import KeybindingsHelper
 from autopilot.emulators.unity import get_state_by_path, make_introspection_object
-from autopilot.emulators.X11 import Keyboard
+from autopilot.emulators.X11 import Keyboard, Mouse
 
 # even though we don't use these directly, we need to make sure they've been
 # imported so the classes contained are registered with the introspection API.
@@ -27,6 +27,7 @@ class Switcher(KeybindingsHelper):
     def __init__(self):
         super(Switcher, self).__init__()
         self._keyboard = Keyboard()
+        self._mouse = Mouse()
 
     def initiate(self):
         """Start the switcher with alt+tab."""
@@ -72,6 +73,18 @@ class Switcher(KeybindingsHelper):
         """Move to the previous application."""
         logger.debug("Selecting previous item in switcher.")
         self.keybinding("switcher/prev")
+
+    def next_icon_mouse(self):
+        """Move to the next application using the mouse scroll wheel"""
+        logger.debug("Selecting next item in switcher with mouse scroll wheel.")
+        self._mouse.press(6)
+        self._mouse.release(6)
+
+    def previous_icon_mouse(self):
+        """Move to the previous application using the mouse scroll wheel"""
+        logger.debug("Selecting previous item in switcher with mouse scroll wheel.")
+        self._mouse.press(7)
+        self._mouse.release(7)
 
     def show_details(self):
         """Show detail mode."""
--- tests/autopilot/autopilot/tests/test_switcher.py
+++ tests/autopilot/autopilot/tests/test_switcher.py
@@ -71,6 +71,59 @@ class SwitcherTests(AutopilotTestCase):
         self.assertThat(end, Equals(start - 1))
         self.set_timeout_setting(True)
 
+    def test_switcher_scroll_next(self):
+        self.set_timeout_setting(False)
+        sleep(1)
+
+        self.switcher.initiate()
+        sleep(.2)
+
+        start = self.switcher.get_selection_index()
+        self.switcher.next_icon_mouse()
+        sleep(.2)
+
+        end = self.switcher.get_selection_index()
+        self.assertThat(start, NotEquals(0))
+        self.assertThat(end, Equals(start + 1))
+
+        # Quickly repeatead events should be ignored (except the first)
+        start = self.switcher.get_selection_index()
+        self.switcher.next_icon_mouse()
+        self.switcher.next_icon_mouse()
+        sleep(.2)
+
+        end = self.switcher.get_selection_index()
+        self.assertThat(end, Equals(start + 1))
+
+        self.switcher.terminate()
+
+    def test_switcher_scroll_prev(self):
+        self.set_timeout_setting(False)
+        sleep(1)
+
+        self.switcher.initiate()
+        sleep(.2)
+
+        start = self.switcher.get_selection_index()
+        self.switcher.previous_icon_mouse()
+        sleep(.2)
+
+        end = self.switcher.get_selection_index()
+        self.assertThat(start, NotEquals(0))
+        self.assertThat(end, Equals(start - 1))
+
+        # Quickly repeatead events should be ignored (except the first)
+        start = self.switcher.get_selection_index()
+        self.switcher.previous_icon_mouse()
+        self.switcher.previous_icon_mouse()
+        sleep(.2)
+
+        end = self.switcher.get_selection_index()
+        self.assertThat(end, Equals(start - 1))
+
+        self.switcher.terminate()
+        self.set_timeout_setting(True)
+
     def test_switcher_starts_in_normal_mode(self):
         """Switcher must start in normal (i.e.- not details) mode."""
         self.switcher.initiate()
