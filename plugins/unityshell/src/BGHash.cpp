eb94ab23b4a99dde2ad4a69574459bcb006120e6
Ryan Lortie <desrt@desrt.ca>
2012-03-09 23:48:59 -0500

    don't store average-bg-color in GSettings
    This is now exported by libgnome-desktop as an X property on the root
    window.  Interested applications can pick it up from there.
    
    (bzr r2065.5.1)

--- plugins/unityshell/src/BGHash.cpp
+++ plugins/unityshell/src/BGHash.cpp
@@ -35,8 +35,6 @@ namespace {
   int level_of_recursion;
   const int MAX_LEVEL_OF_RECURSION = 16;
   const int MIN_LEVEL_OF_RECURSION = 2;
-  std::string AVG_BG_COLOR = "average-bg-color";
-  std::string UNITY_SCHEMA = "com.canonical.Unity";
 }
 namespace unity {
 
@@ -343,23 +341,6 @@ namespace unity {
     _hires_time_start = g_get_monotonic_time();
     _hires_time_end = 500 * 1000; // 500 milliseconds
     _transition_handler = g_timeout_add (1000/60, (GSourceFunc)BGHash::OnTransitionCallback, this);
-
-    // export to gsettings
-    GSettings* settings = NULL;
-    GdkColor   color    = {0,
-                           (guint16) (_new_color.red * 65535.0 * 0.7f),
-                           (guint16) (_new_color.green * 65535.0 * 0.7f),
-                           (guint16) (_new_color.blue * 65535.0 * 0.7f)};
-
-    settings = g_settings_new (UNITY_SCHEMA.c_str());
-    if (settings)
-    {
-      unity::glib::String color_string(gdk_color_to_string(&color));
-      LOG_DEBUG(logger) << "Setting gsettings key to: " << color_string;
-      g_settings_set_string(settings, AVG_BG_COLOR.c_str(), color_string);
-      g_object_unref (settings);
-    }
-
   }
 
   gboolean BGHash::OnTransitionCallback(BGHash *self)
