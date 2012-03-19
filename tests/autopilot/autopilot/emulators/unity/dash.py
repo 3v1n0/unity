fdd2e3a97785ed2e1e76ac5f2297afb42effe906
Tim Penhey <tim.penhey@canonical.com>
2012-03-20 10:34:03 +1300

    Add simple property for the dash search string.
    (bzr r2124.2.6)

--- tests/autopilot/autopilot/emulators/unity/dash.py
+++ tests/autopilot/autopilot/emulators/unity/dash.py
@@ -80,6 +80,10 @@ class Dash(KeybindingsHelper):
         """
         return self.controller.visible
 
+    @property
+    def search_string(self):
+        return self.get_searchbar().search_string
+
     def get_searchbar(self):
         """Returns the searchbar attached to the dash."""
         return self.view.get_searchbar()
