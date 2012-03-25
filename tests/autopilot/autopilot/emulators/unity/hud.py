d47e9f272afc3ba77cc425cbec654fc1df4c29ed
Marco Trevisan (Treviño) <mail@3v1n0.net>
2012-03-25 22:26:39 +0200

    autopilot, hud emulator: removing deprecated get_geometry method
    (bzr r2096.1.31)

--- tests/autopilot/autopilot/emulators/unity/hud.py
+++ tests/autopilot/autopilot/emulators/unity/hud.py
@@ -35,9 +35,6 @@ class HudController(UnityIntrospectionObject, KeybindingsHelper):
         """Tap the 'Alt' key to toggle the hud visibility."""
         self.keybinding("hud/reveal", tap_delay)
 
-    def get_geometry(self):
-        return (self.x, self.y, self.width, self.height)
-
     def _get_view(self):
         views = self.get_children_by_type(HudView)
         return views[0] if views else None
