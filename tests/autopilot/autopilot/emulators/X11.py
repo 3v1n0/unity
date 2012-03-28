a7ccbfd3996841cf236b0450522967df73f64575
Thomi Richards <thomi.richards@canonical.com>
2012-03-29 11:08:50 +1300

    Raise BlacklistedDriverError sooner, since nvidia chipsets can't even get the monitor name.
    (bzr r2178.2.2)

--- tests/autopilot/autopilot/emulators/X11.py
+++ tests/autopilot/autopilot/emulators/X11.py
@@ -335,13 +335,13 @@ class ScreenGeometry:
     def set_primary_monitor(self, monitor):
         monitor_name = self._default_screen.get_monitor_plug_name(monitor)
 
-        if not monitor_name or len(monitor_name) == 0:
-            raise ValueError('Invalid monitor found')
-
         glxinfo_out = subprocess.check_output("glxinfo")
         for dri in self._blacklisted_drivers:
             if dri in glxinfo_out:
                 raise ScreenGeometry.BlacklistedDriverError('Impossible change the primary monitor for the given driver')
+
+        if not monitor_name or len(monitor_name) == 0:
+            raise ValueError('Invalid monitor found')
 
         ret = os.spawnlp(os.P_WAIT, "xrandr", "xrandr", "--output", monitor_name, "--primary")
 
