fce1a784f30dfc3e911395f433af27e9a08e0c24
Alex Launi <alex.launi@canonical.com>
2012-04-10 19:36:02 -0400

    remove Gio and Glib in globals.py
    (bzr r2254.6.7)

--- tests/autopilot/autopilot/globals.py
+++ tests/autopilot/autopilot/globals.py
@@ -1,10 +1,5 @@
 from compizconfig import Context
 
-# Importing gi.repository in the test was causing testtools to fail to run the tests.
-# This is a work
-gio = Gio
-glib = GLib
-
 global_context = Context()
 
 # this can be set to True, in which case tests will be recorded.
