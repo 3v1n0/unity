4921b5184bc2d281e92e42a78984607dca340817
Bilal Akhtar <bilalakhtar@ubuntu.com>
2012-06-13 19:37:50 -0400

    Semicolon missed, ah well, blame Python.
    (bzr r2396.2.3)

--- launcher/LauncherController.cpp
+++ launcher/LauncherController.cpp
@@ -460,7 +460,7 @@ Controller::Impl::OnLauncherAddRequestSpecial(std::string const& path,
   // Check if desktop file was supplied, or if it's set to SC's agent
   // See https://bugs.launchpad.net/unity/+bug/1002440
   if (path.empty() || path.compare("software-center-agent"))
-    return
+    return;
 
   SoftwareCenterLauncherIcon::Ptr result = CreateSCLauncherIcon(path, aptdaemon_trans_id, icon_path);
 
