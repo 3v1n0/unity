88965e6f48bd081d47fd5e8c7366336d7b6b1541
Marco Trevisan (Treviño) <mail@3v1n0.net>
2012-07-02 22:04:33 +0200

    GLibSignal: rewrite using the variadic templates to generate multiple types
    Also, use std::shared_ptr and boost::noncopyable
    
    (bzr r2463.1.1)

--- UnityCore/GLibSignal.cpp
+++ UnityCore/GLibSignal.cpp
@@ -15,6 +15,7 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *
 * Authored by: Neil Jagdish patel <neil.patel@canonical.com>
+*              Marco Trevisan <marco.trevisan@canonical.com>
 */
 
 #include "GLibSignal.h"
@@ -63,8 +64,12 @@ SignalManager::SignalManager()
 // opportunity for random bugs, it also made the API bad.
 void SignalManager::Add(SignalBase* signal)
 {
-  SignalBase::Ptr s(signal);
-  connections_.push_back(s);
+  Add(SignalBase::Ptr(signal));
+}
+
+void SignalManager::Add(SignalBase::Ptr const& signal)
+{
+  connections_.push_back(signal);
 }
 
 // This uses void* to keep in line with the g_signal* functions
