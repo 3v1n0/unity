1b1012a9b80b71ab78882cb97574deaf2009bcc2
Brandon Schaefer <brandontschaefer@gmail.com>
2012-07-06 15:36:27 -0700

    * No keynav focus when mouse down for window buttons! Ever!
    (bzr r2470.6.3)

--- panel/WindowButtons.cpp
+++ panel/WindowButtons.cpp
@@ -46,6 +46,7 @@ public:
     , _overlay_is_open(false)
     , _opacity(1.0f)
   {
+    SetAcceptKeyNavFocusOnMouseDown(false);
     panel::Style::Instance().changed.connect(sigc::mem_fun(this, &WindowButton::LoadImages));
     LoadImages();
   }
@@ -200,7 +201,6 @@ public:
 
     _overlay_is_open = open;
 
-    SetAcceptKeyNavFocusOnMouseDown(!_overlay_is_open);
     UpdateSize();
     QueueDraw();
   }
