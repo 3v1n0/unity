12e344775e39a4d68bf82e1562b880d2dd1cf70a
MC Return <mc.return@gmx.net>
2012-07-30 19:17:21 +0200

    Reduced the scope of the variable 'key_code' in unityshell.cpp
    (bzr r2528.1.18)

--- plugins/unityshell/src/unityshell.cpp
+++ plugins/unityshell/src/unityshell.cpp
@@ -2166,10 +2166,9 @@ bool UnityScreen::ShowHudInitiate(CompAction* action,
 {
   // Look to see if there is a keycode.  If there is, then this isn't a
   // modifier only keybinding.
-  int key_code = 0;
   if (options[6].type() != CompOption::TypeUnset)
   {
-    key_code = options[6].value().i();
+    int key_code = options[6].value().i();
     LOG_DEBUG(logger) << "HUD initiate key code: " << key_code;
     // show it now, no timings or terminate needed.
     return ShowHud();
