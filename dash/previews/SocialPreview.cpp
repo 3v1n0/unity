3211ec92b4bb57a15c9493ef4162b1d59779a1af
Ken VanDine <ken.vandine@canonical.com>
2012-09-06 11:47:18 -0400

    set comments_ width based on the details width
    (bzr r2635.3.22)

--- dash/previews/SocialPreview.cpp
+++ dash/previews/SocialPreview.cpp
@@ -266,7 +266,7 @@ void SocialPreview::PreLayoutManagement()
 
   if (title_) { title_->SetMaximumWidth(top_social_info_max_width); }
   if (subtitle_) { subtitle_->SetMaximumWidth(top_social_info_max_width); }
-  if (comments_) { comments_->SetMaximumWidth(top_social_info_max_width); }
+  if (comments_) { comments_->SetMaximumWidth(details_width); }
 
   for (nux::AbstractButton* button : action_buttons_)
   {
