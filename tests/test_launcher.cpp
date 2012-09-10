c245a7ae616c5ab9d8698742b7e8452b87a556bb
Marco Trevisan (Treviño) <mail@3v1n0.net>
2012-09-10 20:19:19 +0200

    TestLauncher: add test to check drag icon when setting an icon back to its position
    (bzr r2529.14.60)

--- tests/test_launcher.cpp
+++ tests/test_launcher.cpp
@@ -92,6 +92,11 @@ public:
       Launcher::ShowDragWindow();
     }
 
+    void EndIconDrag()
+    {
+      Launcher::EndIconDrag();
+    }
+
     void UpdateDragWindowPosition(int x, int y)
     {
       Launcher::UpdateDragWindowPosition(x, y);
@@ -134,11 +139,10 @@ public:
     for (unsigned i = 0; i < number; ++i)
     {
       MockMockLauncherIcon::Ptr icon(new MockMockLauncherIcon);
-      model_->AddIcon(icon);
-      // Set the icon centers
       icon->SetCenter(nux::Point3(icon_size/2.0f, icon_size/2.0f * (i+1) + 1, 0), monitor, launcher_geo);
 
       icons.push_back(icon);
+      model_->AddIcon(icon);
     }
 
     return icons;
@@ -275,6 +279,92 @@ TEST_F(TestLauncher, DragLauncherIconCancelRestoresIconOrder)
   EXPECT_FALSE(model_saved);
 
   launcher_->HideDragWindow();
+
+  // Let's wait the drag icon animation to be completed
+  Utils::WaitForTimeout(1);
+  EXPECT_EQ(launcher_->GetDraggedIcon(), nullptr);
+}
+
+TEST_F(TestLauncher, DragLauncherIconSavesIconOrderIfPositionHasChanged)
+{
+  auto const& icons = AddMockIcons(3);
+  ASSERT_EQ(icons.size(), 3);
+
+  auto const& icon1 = icons[0];
+  auto const& icon2 = icons[1];
+  auto const& icon3 = icons[2];
+
+  // Start dragging icon2
+  launcher_->StartIconDrag(icon2);
+  launcher_->ShowDragWindow();
+  ASSERT_EQ(launcher_->GetDragIconPosition(), model_->IconIndex(icon2));
+
+  // Moving icon2 at the end
+  auto const& center3 = icon3->GetCenter(launcher_->monitor());
+  launcher_->UpdateDragWindowPosition(center3.x, center3.y);
+
+  bool model_saved = false;
+  model_->saved.connect([&model_saved] { model_saved = true; });
+
+  ASSERT_NE(launcher_->GetDragIconPosition(), model_->IconIndex(icon2));
+  launcher_->EndIconDrag();
+
+  // The icon order should be reset
+  auto it = model_->begin();
+  ASSERT_EQ(*it, icon1); it++;
+  ASSERT_EQ(*it, icon3); it++;
+  ASSERT_EQ(*it, icon2);
+
+  EXPECT_TRUE(model_saved);
+
+  // Let's wait the drag icon animation to be completed
+  Utils::WaitForTimeout(1);
+  EXPECT_EQ(launcher_->GetDraggedIcon(), nullptr);
+}
+
+TEST_F(TestLauncher, DragLauncherIconSavesIconOrderIfPositionHasNotChanged)
+{
+  auto const& icons = AddMockIcons(3);
+  ASSERT_EQ(icons.size(), 3);
+
+  auto const& icon1 = icons[0];
+  auto const& icon2 = icons[1];
+  auto const& icon3 = icons[2];
+
+  // Start dragging icon2
+  launcher_->StartIconDrag(icon2);
+  launcher_->ShowDragWindow();
+  ASSERT_EQ(launcher_->GetDragIconPosition(), model_->IconIndex(icon2));
+
+  // Moving icon2 at the end
+  auto center3 = icon3->GetCenter(launcher_->monitor());
+  launcher_->UpdateDragWindowPosition(center3.x, center3.y);
+
+  // Swapping the centers
+  icon3->SetCenter(icon2->GetCenter(launcher_->monitor()), launcher_->monitor(), launcher_->GetGeometry());
+  icon2->SetCenter(center3, launcher_->monitor(), launcher_->GetGeometry());
+
+  // Moving icon2 back to the middle
+  center3 = icon3->GetCenter(launcher_->monitor());
+  launcher_->UpdateDragWindowPosition(center3.x, center3.y);
+
+  bool model_saved = false;
+  model_->saved.connect([&model_saved] { model_saved = true; });
+
+  ASSERT_EQ(launcher_->GetDragIconPosition(), model_->IconIndex(icon2));
+  launcher_->EndIconDrag();
+
+  // The icon order should be reset
+  auto it = model_->begin();
+  ASSERT_EQ(*it, icon1); it++;
+  ASSERT_EQ(*it, icon2); it++;
+  ASSERT_EQ(*it, icon3);
+
+  EXPECT_FALSE(model_saved);
+
+  // Let's wait the drag icon animation to be completed
+  Utils::WaitForTimeout(1);
+  EXPECT_EQ(launcher_->GetDraggedIcon(), nullptr);
 }
 
 }
