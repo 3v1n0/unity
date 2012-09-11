2181f9dfb98d77e823e3c86f7e370db8ceb93bd4
Tim Penhey <tim.penhey@canonical.com>
2012-09-12 10:56:30 +1200

    Destroy the icon textures.
    (bzr r2675.4.2)

--- launcher/StandaloneLauncher.cpp
+++ launcher/StandaloneLauncher.cpp
@@ -64,6 +64,8 @@ int main(int argc, char** argv)
   wt->Run(NULL);
   // Make sure the controller is destroyed before the window thread.
   controller.reset();
+  ::unity::ui::IconRenderer::DestroyTextures();
+
   delete wt;
   return 0;
 }
