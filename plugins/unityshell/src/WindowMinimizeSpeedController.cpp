78a2fb7c2d668c2627a47b3e4c383ab486bf69c5
Tim Penhey <tim.penhey@canonical.com>
2012-09-12 16:08:06 +1200

    Initialise the initial duration for the window minimisation.
    (bzr r2675.5.2)

--- plugins/unityshell/src/WindowMinimizeSpeedController.cpp
+++ plugins/unityshell/src/WindowMinimizeSpeedController.cpp
@@ -41,6 +41,7 @@ WindowMinimizeSpeedController::WindowMinimizeSpeedController()
   , _minimize_speed_threshold(g_settings_get_int(_settings, "minimize-speed-threshold"))
   , _minimize_slow_duration(g_settings_get_int(_settings, "minimize-slow-duration"))
   , _minimize_fast_duration(g_settings_get_int(_settings, "minimize-fast-duration"))
+  , _duration(200) // going to be overridden anyway, but at least it is initialised
 {
   _minimize_count_changed.Connect(_settings, "changed::minimize-count",
                                   [&] (GSettings*, gchar* name) {
@@ -74,7 +75,7 @@ void WindowMinimizeSpeedController::UpdateCount()
 
 int WindowMinimizeSpeedController::getDuration()
 {
-  return mDuration;
+  return _duration;
 }
 
 void WindowMinimizeSpeedController::SetDuration()
@@ -82,27 +83,27 @@ void WindowMinimizeSpeedController::SetDuration()
   /* Perform some sanity checks on the configuration values */
   if (_minimize_fast_duration > _minimize_slow_duration)
   {
-    LOG_WARN(logger) << "Configuration mismatch: minimize-fast-duration (" 
+    LOG_WARN(logger) << "Configuration mismatch: minimize-fast-duration ("
                       << _minimize_fast_duration
                       << ") is longer than minimize-slow-duration ("
                       << _minimize_slow_duration << "). Not changing speed.";
     return;
   }
-  
+
   if (_minimize_count < 0)
     _minimize_count = 0;
   if (_minimize_count > _minimize_speed_threshold)
     _minimize_count = _minimize_speed_threshold;
- 
+
   /* Adjust the speed so that it gets linearly closer to maximum speed as we
      approach the threshold */
   int speed_range = _minimize_slow_duration - _minimize_fast_duration;
   float position = (_minimize_speed_threshold <= 0) ? 1.0 :
                    static_cast<float>(_minimize_count) / _minimize_speed_threshold;
   int duration = _minimize_slow_duration - std::ceil(position * speed_range);
-  
-  if (duration != mDuration) {
-    mDuration = duration;
+
+  if (duration != _duration) {
+    _duration = duration;
     DurationChanged.emit();
-  } 
+  }
 }
