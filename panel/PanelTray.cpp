0ac6531a72f3b9ddb0f61322b122b2d37274e902
Andrea Azzarone <azzaronea@gmail.com>
2012-09-14 12:12:18 +0200

    Add missing braces.
    (bzr r2686.1.5)

--- panel/PanelTray.cpp
+++ panel/PanelTray.cpp
@@ -30,7 +30,7 @@ namespace
 nux::logging::Logger logger("unity.panel");
 
 const int PADDING = 3;
-const std::array<std::string, 2> WHITELIST = { "JavaEmbeddedFrame", "Wine" };
+const std::array<std::string, 2> WHITELIST {{ "JavaEmbeddedFrame", "Wine" }};
 }
 
 namespace unity
