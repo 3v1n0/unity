e4f2949b813b6437ba7a5a6ff1bf02f748a23c9f
Marco Trevisan (Treviño) <mail@3v1n0.net>
2012-09-18 05:18:11 +0200

    LauncherIcon: remove the debugging text
    (bzr r2529.14.148)

--- launcher/LauncherIcon.cpp
+++ launcher/LauncherIcon.cpp
@@ -518,7 +518,7 @@ LauncherIcon::ShowTooltip()
 
   if (!_tooltip)
     LoadTooltip();
-  _tooltip->SetText(tooltip_text() +" "+ std::to_string(_sort_priority));
+  _tooltip->SetText(tooltip_text());
   _tooltip->ShowTooltipWithTipAt(tip_x, tip_y);
   _tooltip->ShowWindow(!tooltip_text().empty());
   tooltip_visible.emit(_tooltip);
