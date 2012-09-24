2fc620b098667caa362198a038ddbe023f982dfd
Marco Trevisan (Treviño) <mail@3v1n0.net>
2012-09-24 17:39:17 +0200

    autopilot: add SpreadTests class to perform window closing tests
    (bzr r2713.1.21)

---
+++ tests/autopilot/unity/tests/test_spread.py
@@ -0,0 +1,97 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+# Copyright 2012 Canonical
+# Author: Marco Trevisan (Treviño)
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+
+from __future__ import absolute_import
+
+from autopilot.matchers import Eventually
+import logging
+from time import sleep
+from testtools.matchers import Equals, NotEquals
+
+from unity.tests import UnityTestCase
+from unity.emulators.screen import Screen
+
+
+class SpreadTests(UnityTestCase):
+
+    def initiate_spread_for_screen(self):
+        self.addCleanup(self.keybinding, "spread/cancel")
+        self.keybinding("spread/start")
+        sleep(1)
+        self.assertThat(self.window_manager.scale_active, Eventually(Equals(True)))
+
+    def start_test_application_windows(self, app_name, num_windows=2):
+        self.close_all_app(app_name)
+        windows = []
+
+        for i in range(num_windows):
+            win = self.start_app_window(app_name)
+            if len(windows):
+                self.assertThat(win.application, Equals(windows[-1].application))
+
+            windows.append(win)
+
+        self.assertThat(len(windows), Equals(num_windows))
+
+        return windows
+
+    def assertWindowIsNotScaled(self, xid):
+        refresh_fn = lambda: xid in [w.xid for w in self.screen.scaled_windows]
+        self.assertThat(refresh_fn, Eventually(Equals(False)))
+
+    def assertWindowIsClosed(self, xid):
+        refresh_fn = lambda: xid in [w.x_id for w in self.bamf.get_open_windows()]
+        self.assertThat(refresh_fn, Eventually(Equals(False)))
+
+
+    def initiate_spread_for_application(self, desktop_id):
+        icon = self.launcher.model.get_icon(desktop_id=desktop_id)
+        self.assertThat(lambda: icon, Eventually(NotEquals(None)))
+        launcher = self.launcher.get_launcher_for_monitor(self.screen_geo.get_primary_monitor())
+
+        self.addCleanup(self.keybinding, "spread/cancel")
+        launcher.click_launcher_icon(icon)
+        self.assertThat(self.window_manager.scale_active_for_group, Eventually(Equals(True)))
+
+    def test_scale_application_windows(self):
+        [win1, win2] = self.start_test_application_windows("Calculator")
+        self.initiate_spread_for_application(win1.application.desktop_file)
+
+        self.assertThat(lambda: len(self.screen.scaled_windows), Eventually(Equals(2)))
+        self.assertThat(lambda: (win1.x_id and win2.x_id) in [w.xid for w in self.screen.scaled_windows],
+                        Eventually(Equals(True)))
+
+    def test_scaled_window_closes_on_middle_click(self):
+        win = self.start_test_application_windows("Calculator", 2)[0]
+        self.initiate_spread_for_application(win.application.desktop_file)
+
+        target_xid = win.x_id
+        [target_win] = [w for w in self.screen.scaled_windows if w.xid == target_xid]
+
+        (x, y, w, h) = target_win.geometry
+        self.mouse.move(x + w / 2, y + h / 2)
+        sleep(.5)
+        self.mouse.click(button=2)
+
+        self.assertWindowIsNotScaled(target_xid)
+        self.assertWindowIsClosed(target_xid)
+
+    def test_scaled_window_closes_on_close_button_click(self):
+        win = self.start_test_application_windows("Calculator", 1)[0]
+        self.initiate_spread_for_screen()
+
+        target_xid = win.x_id
+        [target_win] = [w for w in self.screen.scaled_windows if w.xid == target_xid]
+
+        (x, y, w, h) = target_win.scale_close_geometry
+        self.mouse.move(x + w / 2, y + h / 2)
+        sleep(.5)
+        self.mouse.click()
+
+        self.assertWindowIsNotScaled(target_xid)
+        self.assertWindowIsClosed(target_xid)
