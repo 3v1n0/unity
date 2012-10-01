ce0eb0e4b8364404d3c3c643f1d4a391f48fbd32
Neil Jagdish Patel <neil.patel@canonical.com>
2012-10-01 15:39:51 +0100

    cleanup
    (bzr r2740.5.3)

--- plugins/unityshell/src/unityshell.cpp
+++ plugins/unityshell/src/unityshell.cpp
@@ -204,10 +204,10 @@ UnityScreen::UnityScreen(CompScreen* screen)
     //In case of software rendering then enable lowgfx mode.
   std::string renderer = ANSI_TO_TCHAR(NUX_REINTERPRET_CAST(const char *, glGetString(GL_RENDERER)));
   
-  if ((strstr(renderer.c_str(), "Software Rasterizer")) ||
-      (strstr(renderer.c_str(), "Mesa X11")) ||
-      (strstr(renderer.c_str(), "LLVM")) ||
-      (strstr(renderer.c_str(), "on softpipe")) ||
+  if (strstr(renderer.c_str(), "Software Rasterizer") ||
+      strstr(renderer.c_str(), "Mesa X11") ||
+      strstr(renderer.c_str(), "LLVM") ||
+      strstr(renderer.c_str(), "on softpipe") ||
       (getenv("UNITY_LOW_GFX_MODE") != NULL && atoi(getenv("UNITY_LOW_GFX_MODE")) == 1))
       {
         Settings::Instance().SetLowGfxMode(true);
