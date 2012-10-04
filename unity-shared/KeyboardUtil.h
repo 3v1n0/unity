2364d3844c9315be63454bc3ce299f041938e643
Tim Penhey <tim.penhey@canonical.com>
2012-10-04 20:42:43 +1300

    Keyboard functions.
    (bzr r2791.5.2)

---
+++ unity-shared/KeyboardUtil.h
@@ -0,0 +1,35 @@
+// -*- Mode: C++; indent-tabs-mode: nil; tab-width: 2 -*-
+/*
+ * Copyright (C) 2011-2012 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Tim Penhey <tim.penhey@canonical.com>
+ */
+
+#ifndef UNITYSHARED_KEYBOARDUTIL_H
+#define UNITYSHARED_KEYBOARDUTIL_H
+
+namespace unity
+{
+namespace keyboard
+{
+
+bool is_printable_key_symbol(unsigned long key_symbol);
+bool is_move_key_symbol(unsigned long key_symbol);
+
+}
+}
+
+#endif // UNITYSHARED_KEYBOARDUTIL_H
+
