908c82d35962eb51ad7d2994f30c57ab5cbebc66
Michal Hruby <michal.mhr@gmail.com>
2012-10-11 14:16:23 +0200

    Add more tests
    (bzr r2769.4.4)

--- tests/test_ubus.cpp
+++ tests/test_ubus.cpp
@@ -66,6 +66,36 @@ struct TestUBusServer : public testing::Test
   }
 };
 
+struct TestUBusManager : public testing::Test
+{
+  UBusManager ubus_manager;
+  bool callback_called;
+  unsigned callback_call_count;
+  glib::Variant last_msg_variant;
+
+  virtual void SetUp()
+  {
+    callback_called = false;
+    callback_call_count = 0;
+  }
+
+  void Callback(glib::Variant const& message)
+  {
+    callback_called = true;
+    callback_call_count++;
+
+    last_msg_variant = message;
+  }
+
+  void ProcessMessages()
+  {
+    bool expired = false;
+    glib::Idle idle([&] { expired = true; return false; },
+                    glib::Source::Priority::LOW);
+    Utils::WaitUntil(expired);
+  }
+};
+
 // UBus tests
 
 TEST_F(TestUBusServer, Contruct)
@@ -154,6 +184,144 @@ TEST_F(TestUBusServer, MultipleDispatchesWithData)
 
   ProcessMessages();
   EXPECT_EQ(cb_count, 2);
+}
+
+TEST_F(TestUBusServer, DispatchesInOrder)
+{
+  std::string order;
+
+  ubus_server.RegisterInterest(MESSAGE1, [&order] (glib::Variant const&)
+      { order += "1"; });
+  ubus_server.RegisterInterest(MESSAGE1, [&order] (glib::Variant const&)
+      { order += "2"; });
+  ubus_server.RegisterInterest(MESSAGE1, [&order] (glib::Variant const&)
+      { order += "3"; });
+  ubus_server.RegisterInterest(MESSAGE2, [&order] (glib::Variant const&)
+      { order += "4"; });
+
+  ubus_server.SendMessage(MESSAGE1);
+  ubus_server.SendMessage(MESSAGE2);
+
+  ProcessMessages();
+  EXPECT_EQ(order, "1234");
+
+  // prepare for second round of dispatches
+  order = "";
+
+  ubus_server.RegisterInterest(MESSAGE2, [&order] (glib::Variant const&)
+      { order += "5"; });
+
+  // swap order of the messages
+  ubus_server.SendMessage(MESSAGE2);
+  ubus_server.SendMessage(MESSAGE1);
+
+  ProcessMessages();
+  EXPECT_EQ(order, "45123");
+}
+
+TEST_F(TestUBusServer, DispatchesWithPriority)
+{
+  std::string order;
+
+  ubus_server.RegisterInterest(MESSAGE1, [&order] (glib::Variant const&)
+      { order += "1"; });
+  ubus_server.RegisterInterest(MESSAGE1, [&order] (glib::Variant const&)
+      { order += "2"; });
+  ubus_server.RegisterInterest(MESSAGE1, [&order] (glib::Variant const&)
+      { order += "3"; });
+  ubus_server.RegisterInterest(MESSAGE2, [&order] (glib::Variant const&)
+      { order += "4"; });
+  ubus_server.RegisterInterest(MESSAGE2, [&order] (glib::Variant const&)
+      { order += "5"; });
+
+  glib::Variant data;
+  ubus_server.SendMessage(MESSAGE1, data);
+  ubus_server.SendMessageFull(MESSAGE2, data, glib::Source::Priority::HIGH);
+
+  ProcessMessages();
+  EXPECT_EQ(order, "45123");
+}
+
+TEST_F(TestUBusManager, RegisterAndSend)
+{
+  ubus_manager.RegisterInterest(MESSAGE1, sigc::mem_fun(this, &TestUBusManager::Callback));
+
+  UBusManager::SendMessage(MESSAGE1);
+
+  ProcessMessages();
+  EXPECT_TRUE(callback_called);
+  EXPECT_EQ(callback_call_count, 1);
+}
+
+TEST_F(TestUBusManager, Unregister)
+{
+  auto interest_id = ubus_manager.RegisterInterest(MESSAGE1, 
+      sigc::mem_fun(this, &TestUBusManager::Callback));
+
+  UBusManager::SendMessage(MESSAGE1);
+
+  ubus_manager.UnregisterInterest(interest_id);
+
+  ProcessMessages();
+  EXPECT_FALSE(callback_called);
+}
+
+TEST_F(TestUBusManager, AutoUnregister)
+{
+  if (true)
+  {
+    // want this to go out of scope
+    UBusManager manager;
+
+    manager.RegisterInterest(MESSAGE1,
+                             sigc::mem_fun(this, &TestUBusManager::Callback));
+  }
+
+  UBusManager::SendMessage(MESSAGE1);
+
+  ProcessMessages();
+  EXPECT_FALSE(callback_called);
+}
+
+TEST_F(TestUBusManager, UnregisterInsideCallback)
+{
+  unsigned interest_id;
+  interest_id = ubus_manager.RegisterInterest(MESSAGE1, [&] (glib::Variant const& data)
+  {
+    callback_called = true;
+    callback_call_count++;
+    ubus_manager.UnregisterInterest(interest_id);
+  });
+
+  UBusManager::SendMessage(MESSAGE1);
+  UBusManager::SendMessage(MESSAGE1);
+
+  ProcessMessages();
+  EXPECT_TRUE(callback_called);
+  EXPECT_EQ(callback_call_count, 1);
+}
+
+TEST_F(TestUBusManager, DispatchWithPriority)
+{
+  std::string order;
+
+  ubus_manager.RegisterInterest(MESSAGE1, [&order] (glib::Variant const&)
+      { order += "1"; });
+  ubus_manager.RegisterInterest(MESSAGE1, [&order] (glib::Variant const&)
+      { order += "2"; });
+  ubus_manager.RegisterInterest(MESSAGE1, [&order] (glib::Variant const&)
+      { order += "3"; });
+  ubus_manager.RegisterInterest(MESSAGE2, [&order] (glib::Variant const&)
+      { order += "4"; });
+  ubus_manager.RegisterInterest(MESSAGE2, [&order] (glib::Variant const&)
+      { order += "5"; });
+
+  glib::Variant data;
+  UBusManager::SendMessage(MESSAGE1, data);
+  UBusManager::SendMessage(MESSAGE2, data, glib::Source::Priority::HIGH);
+
+  ProcessMessages();
+  EXPECT_EQ(order, "45123");
 }
 
 }
