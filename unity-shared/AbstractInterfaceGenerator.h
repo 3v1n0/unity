c405ddd748ff9836ef282f1de757347cb72018f6
Sam Spilsbury <sam.spilsbury@canonical.com>
2012-12-05 15:07:06 +0800

    Added AbstractInterfaceGenerator, which is a helper template to visit   an interface contained by composition by various concrete objects in a   collection (eg, if the UnityWindow instance for each window in a   CompWindowList had some interface XInterface that you wanted to use, you   could do this):
      unity::AbstractInterfaceCollectionGenerator generator <const XInterface &, Com$
      
      foo (&generator);
      
      void foo (const unity::AbstractInterfaceGenerator <const XInterface &> &gen)
      {
        gen.VisitEachInterface ([](const XInterface &iface) { iface.x (); }) 
      }
    
    (bzr r2948.4.1)

---
+++ unity-shared/AbstractInterfaceGenerator.h
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the  Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 3 along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>
+ *
+ * Authored by: Sam Spilsbury <sam.spilsbury@canonical.com>
+ *
+ */
+#ifndef _UNITY_ABSTRACT_INTERFACE_GENERATOR_H
+#define _UNITY_ABSTRACT_INTERFACE_GENERATOR_H
+
+#include <memory>
+#include <functional>
+
+namespace unity
+{
+namespace internal
+{
+template <class ConcreteCollection>
+class ReverseWrapper
+{
+  private:
+
+    const ConcreteCollection &collection_;
+
+  public:
+
+    ReverseWrapper (const ConcreteCollection &collection) :
+      collection_ (collection)
+    {
+    }
+
+    decltype (collection_.rbegin ()) begin () const { return collection_.rbegin (); }
+    decltype (collection_.rend ()) end () const { return collection_.rend (); }
+};
+}
+
+/*
+ * This can be passed to objects which want to visit some interfaces
+ * they can use from a collection of concrete objects they do not need
+ * to know about
+ */
+template <class AbstractInterface>
+class AbstractInterfaceGenerator
+{
+  public:
+
+    typedef std::shared_ptr <AbstractInterfaceGenerator<AbstractInterface> > Ptr;
+    typedef std::function <void (AbstractInterface &)> InterfaceVisitor;
+    virtual ~AbstractInterfaceGenerator () {}
+
+    virtual void VisitEachInterface (const InterfaceVisitor &visit) = 0;
+    virtual void VisitEachInterfaceReverse (const InterfaceVisitor &visit) = 0;
+};
+
+/*
+ * This is a helper template to retrieve abstract interfaces from collections
+ * containing concrete types. For example a CompWindowList or CompWindowVector
+ * for which you might want to retreive a relevant FooInterface from each
+ * element.
+ */
+template <class AbstractInterface, class ConcreteCollection>
+class AbstractInterfaceCollectionGenerator :
+  public AbstractInterfaceGenerator <AbstractInterface>
+{
+  public:
+
+    typedef AbstractInterfaceGenerator<AbstractInterface> InterfaceGenerator;
+    typedef typename InterfaceGenerator::InterfaceVisitor Visitor;
+    typedef std::function <AbstractInterface & (typename ConcreteCollection::value_type &) >
+      ElementRetreivalFunc;
+
+    AbstractInterfaceCollectionGenerator (const ConcreteCollection   &collection,
+                                          const ElementRetreivalFunc &retreival) :
+      collection_ (collection),
+      retreival_ (retreival)
+    {
+    }
+
+    void VisitEachInterface (const Visitor &visit)
+    {
+      VisitEachInterfaceInternal (collection_, visit);
+    }
+
+    void VisitEachInterfaceReverse (const Visitor &visit)
+    {
+      VisitEachInterfaceInternal (internal::ReverseWrapper<ConcreteCollection> (collection_), visit);
+    }
+
+  private:
+
+    template <class Container>
+    void VisitEachInterfaceInternal (const Container &container,
+                                     const Visitor   &visit)
+    {
+      for (const typename ConcreteCollection::value_type &item : container)
+      {
+        /* Someone help */
+        auto non_const_item = const_cast <typename ConcreteCollection::value_type *> (&item);
+        AbstractInterface &interface (retreival_ (*non_const_item));
+        visit (interface);
+      }
+    }
+
+    const ConcreteCollection   &collection_;
+    ElementRetreivalFunc       retreival_;
+};
+}
+
+#endif
