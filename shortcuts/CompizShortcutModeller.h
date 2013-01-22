d017a2d1b3ac40cf6098ea0adc7e75c2a241e7f1
Marco Trevisan (Treviño) <mail@3v1n0.net>
2013-01-22 21:09:26 +0100

    CompizShortcutModeller: implement a basic compiz shortcut Modeller
    (bzr r2919.3.51)

---
+++ shortcuts/CompizShortcutModeller.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2013 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the  Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 3 along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>
+ *
+ * Authored by: Marco Trevisan <marco.trevisan@canonical.com>
+ */
+
+#ifndef UNITYSHELL_COMPIZ_SHORTCUT_MODELLER_H
+#define UNITYSHELL_COMPIZ_SHORTCUT_MODELLER_H
+
+#include "AbstractShortcutModeller.h"
+
+namespace unity
+{
+namespace shortcut
+{
+
+class CompizModeller : public AbstractModeller
+{
+public:
+  CompizModeller();
+  Model::Ptr GetCurrentModel() const {return model_;}
+
+  sigc::signal<void, Model::Ptr const&> model_changed;
+
+private:
+  Model::Ptr model_;
+};
+
+}
+}
+
+#endif
