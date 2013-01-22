b91488e509096e066dcd914d0e0489eaf80d39f2
Marco Trevisan (Treviño) <mail@3v1n0.net>
2013-01-23 00:03:34 +0100

    TestShortcutController: check model update on modeller changes
    (bzr r2919.3.58)

--- tests/test_shortcut_controller.cpp
+++ tests/test_shortcut_controller.cpp
@@ -76,7 +76,8 @@ class TestShortcutController : public Test
 public:
   TestShortcutController()
     : base_window_raiser_(std::make_shared<NiceMock<MockBaseWindowRaiser>>())
-    , controller_(base_window_raiser_, std::make_shared<StandaloneModeller>())
+    , modeller_(std::make_shared<StandaloneModeller>())
+    , controller_(base_window_raiser_, modeller_)
     , animation_controller_(tick_source_)
   {
     ON_CALL(controller_, SetOpacity(_))
@@ -86,13 +87,14 @@ public:
   MockUScreen uscreen;
   Settings unity_settings;
   MockBaseWindowRaiser::Ptr base_window_raiser_;
+  AbstractModeller::Ptr const& modeller_;
   NiceMock<MockShortcutController> controller_;
 
   nux::animation::TickSource tick_source_;
   nux::animation::AnimationController animation_controller_;
 };
 
-TEST_F (TestShortcutController, WindowIsRaisedOnShow)
+TEST_F(TestShortcutController, WindowIsRaisedOnShow)
 {
   EXPECT_CALL(*base_window_raiser_, Raise(_))
     .Times(1);
@@ -101,7 +103,7 @@ TEST_F (TestShortcutController, WindowIsRaisedOnShow)
   Utils::WaitForTimeout(1);
 }
 
-TEST_F (TestShortcutController, HiddeenOnConstruction)
+TEST_F(TestShortcutController, HiddeenOnConstruction)
 {
   {
     InSequence sequence;
@@ -115,7 +117,7 @@ TEST_F (TestShortcutController, HiddeenOnConstruction)
   controller_.Hide();
 }
 
-TEST_F (TestShortcutController, GetGeometryPerMonitor)
+TEST_F(TestShortcutController, GetGeometryPerMonitor)
 {
   nux::Geometry good_monitor(0, 0, 1366, 768);
   nux::Geometry invalid_monitor(good_monitor.x + good_monitor.width, 0, 1, 1);
@@ -131,6 +133,17 @@ TEST_F (TestShortcutController, GetGeometryPerMonitor)
   EXPECT_EQ(controller_.GetGeometryPerMonitor(0), expected);
 
   EXPECT_TRUE(controller_.GetGeometryPerMonitor(1).IsNull());
+}
+
+TEST_F(TestShortcutController, ModelIsChangedOnModellerChange)
+{
+  controller_.ConstructView();
+  auto old_model = controller_.view_->GetModel();
+  auto model = std::make_shared<Model>(std::list<AbstractHint::Ptr>());
+  modeller_->model_changed(model);
+
+  ASSERT_NE(controller_.view_->GetModel(), old_model);
+  EXPECT_EQ(controller_.view_->GetModel(), model);
 }
 
 
