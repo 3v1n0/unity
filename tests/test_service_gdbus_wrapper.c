b0397a3045c461c986a566b6e85c88aecda5cd08
Andrea Azzarone <azzaronea@gmail.com>
2013-01-29 21:03:49 +0100

    Fix typo.
    (bzr r2326.1.2)

--- tests/test_service_gdbus_wrapper.c
+++ tests/test_service_gdbus_wrapper.c
@@ -74,7 +74,7 @@ service_gdbus_wrapper_dispose(GObject* object)
     self->priv->bus = NULL;
   }
 
-  G_OBJECT_CLASS (service_gbus_wrapper_parent_class)->dispose (object);
+  G_OBJECT_CLASS (service_gdbus_wrapper_parent_class)->dispose (object);
 }
 
 static void
