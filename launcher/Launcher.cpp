5e9af3dde43a73feadea2a4a42ce9236a27e539b
Marco Trevisan (Treviño) <mail@3v1n0.net>
2013-03-27 20:36:01 +0100

    Launcher: don't draw any left pip for Trash and Device icons
    (bzr r3212.3.25)

--- launcher/Launcher.cpp
+++ launcher/Launcher.cpp
@@ -851,6 +851,13 @@ void Launcher::SetupRenderArg(AbstractLauncherIcon::Ptr const& icon, struct time
       arg.window_indicators = std::max<int> (icon->WindowsOnViewport().size(), 1);
     else
       arg.window_indicators = std::max<int> (icon->WindowsForMonitor(monitor).size(), 1);
+
+    if (icon->GetIconType() == AbstractLauncherIcon::IconType::TRASH ||
+        icon->GetIconType() == AbstractLauncherIcon::IconType::DEVICE)
+    {
+      // TODO: also these icons should respect the actual windows they have
+      arg.window_indicators = 0;
+    }
   }
 
   arg.backlight_intensity = IconBackgroundIntensity(icon, current);
