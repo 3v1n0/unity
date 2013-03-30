4c92259ebf3f084ccafca173127fab526749296c
Sam Spilsbury <sam.spilsbury@canonical.com>
2013-03-30 22:01:45 +0800

    Unrevert
    (bzr r3248.4.3)

--- unity-shared/BackgroundEffectHelper.h
+++ unity-shared/BackgroundEffectHelper.h
@@ -46,17 +46,19 @@ public:
   nux::Property<nux::View*> owner;
   nux::Property<bool> enabled;
 
-  nux::ObjectPtr<nux::IOpenGLBaseTexture> GetBlurRegion(nux::Geometry const& geo, bool force_update = false);
+  void SetBackbufferRegion(nux::Geometry const& geo);
+  nux::ObjectPtr<nux::IOpenGLBaseTexture> GetBlurRegion(bool force_update = false);
   // We could add more functions here to get different types of effects based on the background texture
   // nux::ObjectPtr<nux::IOpenGLBaseTexture> GetPixelatedRegion(nux::Rect rect, int pixel_size, bool update);
 
-  nux::ObjectPtr<nux::IOpenGLBaseTexture> GetRegion(nux::Geometry const& geometry, bool force_update = false);
+  nux::ObjectPtr<nux::IOpenGLBaseTexture> GetRegion(bool force_update = false);
 
   void DirtyCache();
 
   static void ProcessDamage(nux::Geometry geo);
   static bool HasDirtyHelpers();
   static bool HasEnabledHelpers();
+  static std::vector <nux::Geometry> GetBlurGeometries ();
 
   static nux::Property<unity::BlurType> blur_type;
   static nux::Property<float> sigma_high;
@@ -66,6 +68,8 @@ public:
   static nux::Property<bool> detecting_occlusions;
 
   static nux::Geometry monitor_rect_;
+
+  static sigc::signal<void, nux::Geometry const&> blur_region_needs_update_;
   
   nux::FxStructure blur_fx_struct_;
   nux::FxStructure noise_fx_struct_;
@@ -76,12 +80,14 @@ protected:
 
 private:
   void OnEnabledChanged (bool value);
+  static void ExpandByRadius(nux::Geometry &geometry);
 
   nux::BaseTexture*                       noise_texture_;
   nux::ObjectPtr<nux::IOpenGLBaseTexture> blur_texture_;
   nux::ObjectPtr<nux::IOpenGLBaseTexture> resize_tmp_;
   nux::ObjectPtr<nux::IOpenGLBaseTexture> noisy_tmp_;
   nux::Geometry blur_geometry_;
+  nux::Geometry requested_blur_geometry_;
 
   bool cache_dirty;
 
--- unity-shared/OverlayRenderer.h
+++ unity-shared/OverlayRenderer.h
@@ -54,6 +54,13 @@ public:
   void AboutToHide();
   
   /*
+   * Call this whenever the interface size changes
+   */
+  void UpdateBlurBackgroundSize (nux::Geometry const& content_geo,
+                                 nux::Geometry const& absolute_geo,
+                                 bool                 force_edges);
+
+  /*
    * Disables the blur, if you need it disabled. can not re-enable it.
    */
   void DisableBlur();
