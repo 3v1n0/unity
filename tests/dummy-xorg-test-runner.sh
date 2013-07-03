fa77603dd81fb93bec0e80657b7a3fb579d24a1e
Marco Trevisan (Treviño) <mail@3v1n0.net>
2013-07-04 01:29:38 +0200

    dummy-xorg-test-runner: added a script to run tests in a dummy X server
    Calling ./dummy-xorg-test-runner.sh binary --any-parameter -f --bar will
    load binary (with requested parameters) in a fake X server (using dummy
    xorg video driver, that now also uses llvmpipe to get 3d acceleration)
    
    (bzr r3387.5.17)

---
+++ tests/dummy-xorg-test-runner.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Copyright (C) 2013 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Authored by: Marco Trevisan <marco.trevisan@canonical.com>
+
+binary=$1
+if [ -z "$binary" ]; then echo "Empty command submitted"; exit 1; fi
+if [ ! -x "$binary" ]; then echo "The provided $binary is not executable"; exit 1; fi
+
+shift
+logfile=$(mktemp -t dummy.Xorg.log.XXXXXXXX)
+conffile=$(mktemp -t dummy.Xorg.conf.XXXXXXXX)
+
+cat << 'END_OF_CONFIG' > $conffile
+Section "Monitor"
+        Identifier "Dummy Monitor"
+EndSection
+
+Section "Device"
+        Identifier "Dummy Card"
+        Driver "dummy"
+EndSection
+
+Section "Screen"
+        DefaultDepth 24
+        Identifier "Dummy Screen"
+        Device "Dummy Card"
+        Monitor "Dummy Monitor"
+EndSection
+END_OF_CONFIG
+
+export DISPLAY=:`for id in $(seq 100 150); do test -e /tmp/.X$id-lock || { echo $id; exit 0; }; done; exit 1`
+Xorg $DISPLAY -config $conffile -logfile $logfile &> /dev/null &
+x_pid=$!
+
+$binary $@
+ret_val=$?
+
+kill $x_pid
+rm $conffile
+rm $logfile*
+
+exit $ret_val
