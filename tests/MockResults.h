031aa2f0b885bc5684d9a7c160c6d429afe35ff9
Marco Trevisan (Treviño) <mail@3v1n0.net>
2013-07-09 19:25:50 +0200

    MockResults: remove any dee dependency on MockResults
    Implement RowAtIndex for Result in tests
    
    (bzr r3387.5.46)

--- tests/MockResults.h
+++ tests/MockResults.h
@@ -15,46 +15,28 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Authored by: Andrea Azzarone <andrea.azzarone@canonical.com>
+ *              Marco Trevisan <marco.trevisan@canonical.com>
  */
 
 #ifndef UNITY_MOCK_RESULTS_H
 #define UNITY_MOCK_RESULTS_H
 
-#include "UnityCore/Results.h"
+#include <UnityCore/Results.h>
 
 namespace unity {
 namespace dash {
 
 struct MockResults : public Results
 {
-  MockResults(unsigned int count)
+  MockResults(unsigned int count_)
     : Results(LOCAL)
-    , model_(dee_sequence_model_new())
   {
-    dee_model_set_schema(model_, "s", "s", "u", "u", "s", "s", "s", "s", "a{sv}", nullptr);
-    AddResults(count);
-
-    SetModel(model_);
+    count.SetGetterFunction([count_] { return count_; });
   }
-
-  void AddResults(unsigned count)
-  {
-    GVariantBuilder b;
-    g_variant_builder_init(&b, G_VARIANT_TYPE("a{sv}"));
-    GVariant *hints = g_variant_builder_end(&b);
-
-    for(unsigned i = 0; i < count; ++i)
-    {
-      dee_model_append(model_, "uri", "icon_hint", 
-                       0, 0, "mimetype", 
-                       "title", "comment", "dnd_uri", hints);
-    }
-
-    g_variant_unref(hints);
-  }
-
-  glib::Object<DeeModel> model_;
 };
+
+// Template specialization for Result in tests
+template<> const Result Model<Result>::RowAtIndex(std::size_t index) const;
 
 }
 }
