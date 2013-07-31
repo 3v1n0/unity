70f2f31333509c129b9ac963da84288b6c266cbb
Marco Trevisan (Treviño) <mail@3v1n0.net>
2013-07-31 04:40:28 +0200

    TestSoftwareCenterLauncherIcon: add more checks OnFinished, use TEST_P for progress tests
    (bzr r3440.1.17)

--- tests/test_software_center_launcher_icon.cpp
+++ tests/test_software_center_launcher_icon.cpp
@@ -104,18 +104,23 @@ TEST_F(TestSoftwareCenterLauncherIcon, DesktopFileTransformSCAgent)
 // and ensure that the remote uri is updated from temp location to
 // the real location
 TEST_F(TestSoftwareCenterLauncherIcon, OnFinished)
-{   
-   // now simulate that the install was successful
-   GVariant *params = g_variant_new("(s)", "exit-success");
-   icon.OnFinished(params);
+{
+  bool saved = false;
+  icon.position_saved.connect([&saved] {saved = true;});
 
-   // and verify that both the desktop file and the remote uri gets updated
+  // now simulate that the install was successful
+  GVariant *params = g_variant_new("(s)", "exit-success");
+  icon.OnFinished(params);
 
-   EXPECT_EQ(icon._desktop_file, USC_DESKTOP);
-   EXPECT_EQ(icon.GetRemoteUri(), "application://ubuntu-software-center.desktop");
-   EXPECT_TRUE(usc->closed.empty());
+  // and verify that both the desktop file and the remote uri gets updated
 
-   g_variant_unref(params);
+  EXPECT_EQ(USC_DESKTOP, icon._desktop_file);
+  EXPECT_EQ("application://ubuntu-software-center.desktop", icon.GetRemoteUri());
+  EXPECT_TRUE(usc->closed.empty());
+  EXPECT_TRUE(icon.IsSticky());
+  EXPECT_TRUE(saved);
+
+  g_variant_unref(params);
 }
 
 TEST_F(TestSoftwareCenterLauncherIcon, Animate)
@@ -135,16 +140,21 @@ TEST_F(TestSoftwareCenterLauncherIcon, Animate)
   EXPECT_TRUE(icon.IsVisible());
 }
 
-TEST_F(TestSoftwareCenterLauncherIcon, TooltipInstalling)
+struct InstallProgress : TestSoftwareCenterLauncherIcon, testing::WithParamInterface<int> {};
+INSTANTIATE_TEST_CASE_P(TestSoftwareCenterLauncherIcon, InstallProgress, testing::Range<int>(0, 99, 10));
+
+TEST_P(/*TestSoftwareCenterLauncherIcon*/InstallProgress, InstallEmblems)
 {
   ASSERT_EQ(icon.tooltip_text(), "Waiting to install");
 
-  GVariant *progress = g_variant_new("i", 10);
+  GVariant *progress = g_variant_new("i", GetParam());
   GVariant *params = g_variant_new("(sv)", "Progress", progress);
 
   icon.OnPropertyChanged(params);
 
-  EXPECT_EQ(icon.tooltip_text(), "Installing…");
+  EXPECT_EQ("Installing…", icon.tooltip_text());
+  EXPECT_TRUE(icon.GetQuirk(AbstractLauncherIcon::Quirk::PROGRESS));
+  EXPECT_FLOAT_EQ(GetParam()/100.0f, icon.GetProgress());
 
   g_variant_unref(params);
 }
