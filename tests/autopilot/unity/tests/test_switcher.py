13de6132c8988052f64edc007cd454abc9d11c11
Brandon Schaefer <brandon.schaefer@canonical.com>
2013-08-27 13:32:26 -0700

    * Fix a failing AP test...not counting the mouse up in the swicher...causing it to never fully click. Click twice to ensure one gets through...
    (bzr r3477.4.1)

--- tests/autopilot/unity/tests/test_switcher.py
+++ tests/autopilot/unity/tests/test_switcher.py
@@ -626,6 +626,8 @@ class SwitcherDetailsMouseTests(SwitcherTestCase):
         y = self.unity.switcher.view.detail_icons[0].y + offset
 
         self.mouse.move(x,y)
+
+        self.mouse.click()
         self.mouse.click()
 
         self.assertProperty(char_win1, is_focused=True)
