90a2c347ef9d8470c95e6f85f10237ca1add9afe
Marco Trevisan (Treviño) <mail@3v1n0.net>
2013-09-04 20:08:31 +0100

    TestDesktopApplicationSubject: verify copy constructors
    (bzr r3477.5.32)

--- tests/test_desktop_application_subject.cpp
+++ tests/test_desktop_application_subject.cpp
@@ -20,6 +20,7 @@
 
 #include <gtest/gtest.h>
 #include <unity-shared/DesktopApplicationManager.h>
+#include "mock-application.h"
 
 using namespace unity::desktop;
 
@@ -167,6 +168,55 @@ TEST_P(/*TestDesktopApplicationSubject*/Property, Manifestation)
   changed = false;
   subject.manifestation = GetParam();
   EXPECT_FALSE(changed);
+}
+
+TEST_P(/*TestDesktopApplicationSubject*/Property, CopyConstructor)
+{
+  subject.uri = GetParam() + "uri";
+  subject.origin = GetParam() + "origin";
+  subject.text = GetParam() + "text";
+  subject.storage = GetParam() + "storage";
+  subject.current_uri = GetParam() + "current_uri";
+  subject.current_origin = GetParam() + "current_origin";
+  subject.mimetype = GetParam() + "mimetype";
+  subject.interpretation = GetParam() + "interpretation";
+  subject.manifestation = GetParam() + "manifestation";
+
+  ApplicationSubject copy_subject(subject);
+  EXPECT_EQ(subject.uri(), copy_subject.uri());
+  EXPECT_EQ(subject.origin(), copy_subject.origin());
+  EXPECT_EQ(subject.text(), copy_subject.text());
+  EXPECT_EQ(subject.storage(), copy_subject.storage());
+  EXPECT_EQ(subject.current_uri(), copy_subject.current_uri());
+  EXPECT_EQ(subject.current_origin(), copy_subject.current_origin());
+  EXPECT_EQ(subject.mimetype(), copy_subject.mimetype());
+  EXPECT_EQ(subject.interpretation(), copy_subject.interpretation());
+  EXPECT_EQ(subject.manifestation(), copy_subject.manifestation());
+}
+
+TEST_P(/*TestDesktopApplicationSubject*/Property, CopyBaseTypeConstructor)
+{
+  testmocks::MockApplicationSubject mock_subject;
+  mock_subject.uri = GetParam() + "uri";
+  mock_subject.origin = GetParam() + "origin";
+  mock_subject.text = GetParam() + "text";
+  mock_subject.storage = GetParam() + "storage";
+  mock_subject.current_uri = GetParam() + "current_uri";
+  mock_subject.current_origin = GetParam() + "current_origin";
+  mock_subject.mimetype = GetParam() + "mimetype";
+  mock_subject.interpretation = GetParam() + "interpretation";
+  mock_subject.manifestation = GetParam() + "manifestation";
+
+  ApplicationSubject copy_subject(mock_subject);
+  EXPECT_EQ(mock_subject.uri(), copy_subject.uri());
+  EXPECT_EQ(mock_subject.origin(), copy_subject.origin());
+  EXPECT_EQ(mock_subject.text(), copy_subject.text());
+  EXPECT_EQ(mock_subject.storage(), copy_subject.storage());
+  EXPECT_EQ(mock_subject.current_uri(), copy_subject.current_uri());
+  EXPECT_EQ(mock_subject.current_origin(), copy_subject.current_origin());
+  EXPECT_EQ(mock_subject.mimetype(), copy_subject.mimetype());
+  EXPECT_EQ(mock_subject.interpretation(), copy_subject.interpretation());
+  EXPECT_EQ(mock_subject.manifestation(), copy_subject.manifestation());
 }
 
 } // anonymous namespace
