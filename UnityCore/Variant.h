469196a1d5f68c2140cff2c68b592f6c985b9f46
Marco Trevisan (Treviño) <mail@3v1n0.net>
2013-09-19 15:06:58 +0200

    Variant: update copyright
    (bzr r3506.5.29)

--- UnityCore/Variant.h
+++ UnityCore/Variant.h
@@ -1,6 +1,6 @@
 // -*- Mode: C++; indent-tabs-mode: nil; tab-width: 2 -*-
 /*
- * Copyright (C) 2011 Canonical Ltd
+ * Copyright (C) 2011-2013 Canonical Ltd
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
@@ -15,6 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Authored by: Tim Penhey <tim.penhey@canonical.com>
+ *              Marco Trevisan <marco.trevisan@canonical.com>
  */
 
 #ifndef UNITY_VARIANT_H
