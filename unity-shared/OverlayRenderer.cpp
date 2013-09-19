6d65e1b87579e99adef5a9eb5004a27a9724455f
Stephen M. Webb <stephen.webb@canonical.com>
2013-09-19 08:36:40 -0400

    unity-shared/OverlayRenderer.cpp: fixed copyright dates
    (bzr r3206.1.4)

--- unity-shared/OverlayRenderer.cpp
+++ unity-shared/OverlayRenderer.cpp
@@ -1,6 +1,6 @@
 // -*- Mode: C++; indent-tabs-mode: nil; tab-width: 2 -*-
 /*
- * Copyright (C) 2011 Canonical Ltd
+ * Copyright (C) 2011-2013 Canonical Ltd
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
