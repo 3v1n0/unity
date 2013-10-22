0d49517f10bbb333c754bbbc78994ecf5f78a37b
Marco Trevisan (Treviño) <mail@3v1n0.net>
2013-10-22 14:33:45 +0200

    SingleMonitorLauncherIcon: fix compilation issue
    (bzr r3566.3.46)

--- launcher/SingleMonitorLauncherIcon.cpp
+++ launcher/SingleMonitorLauncherIcon.cpp
@@ -37,7 +37,7 @@ SingleMonitorLauncherIcon::SingleMonitorLauncherIcon(IconType type, int monitor)
 void SingleMonitorLauncherIcon::UpdateMonitor()
 {
   for (unsigned i = 0; i < monitors::MAX; ++i)
-    SetVisibleOnMonitor(i, i == monitor_);
+    SetVisibleOnMonitor(i, static_cast<int>(i) == monitor_);
 }
 
 void SingleMonitorLauncherIcon::SetMonitor(int monitor)
