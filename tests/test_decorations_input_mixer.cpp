6ae085ecafb3f4095f9136d3d2b07d3811c5cadd
Marco Trevisan (Treviño) <mail@3v1n0.net>
2013-12-17 21:16:45 +0100

    TestDecorationInputMixer: add tests for Enter and Motion events
    (bzr r3566.5.84)

--- tests/test_decorations_input_mixer.cpp
+++ tests/test_decorations_input_mixer.cpp
@@ -52,9 +52,17 @@ TEST_F(TestDecorationInputMixer, PushToFrontItem)
   auto item3 = SizedMockItem(20, 20);
   mixer.PushToFront(item3);
 
-  EXPECT_EQ(*std::next(mixer.Items().begin(), 0), item3);
-  EXPECT_EQ(*std::next(mixer.Items().begin(), 1), item2);
-  EXPECT_EQ(*std::next(mixer.Items().begin(), 2), item1);
+  auto const& items = mixer.Items();
+  ASSERT_EQ(3, items.size());
+  EXPECT_EQ(item3, *std::next(items.begin(), 0));
+  EXPECT_EQ(item2, *std::next(items.begin(), 1));
+  EXPECT_EQ(item1, *std::next(items.begin(), 2));
+
+  mixer.PushToFront(item2);
+  ASSERT_EQ(3, mixer.Items().size());
+  EXPECT_EQ(item2, *std::next(items.begin(), 0));
+  EXPECT_EQ(item3, *std::next(items.begin(), 1));
+  EXPECT_EQ(item1, *std::next(items.begin(), 2));
 }
 
 TEST_F(TestDecorationInputMixer, PushToBackItem)
@@ -68,9 +76,127 @@ TEST_F(TestDecorationInputMixer, PushToBackItem)
   auto item3 = SizedMockItem(20, 20);
   mixer.PushToBack(item3);
 
-  EXPECT_EQ(*std::next(mixer.Items().begin(), 0), item1);
-  EXPECT_EQ(*std::next(mixer.Items().begin(), 1), item2);
-  EXPECT_EQ(*std::next(mixer.Items().begin(), 2), item3);
+  auto const& items = mixer.Items();
+  ASSERT_EQ(3, items.size());
+  EXPECT_EQ(item1, *std::next(items.begin(), 0));
+  EXPECT_EQ(item2, *std::next(items.begin(), 1));
+  EXPECT_EQ(item3, *std::next(items.begin(), 2));
+
+  mixer.PushToBack(item2);
+  ASSERT_EQ(3, items.size());
+  EXPECT_EQ(item1, *std::next(items.begin(), 0));
+  EXPECT_EQ(item3, *std::next(items.begin(), 1));
+  EXPECT_EQ(item2, *std::next(items.begin(), 2));
+}
+
+
+TEST_F(TestDecorationInputMixer, EnterEvent)
+{
+  auto item1 = SizedMockItem(30, 30);
+  SigReceiver sig1(*item1);
+  mixer.PushToFront(item1);
+
+  auto item2 = SizedMockItem(20, 20);
+  SigReceiver sig2(*item2);
+  mixer.PushToFront(item2);
+
+  auto item3 = SizedMockItem(10, 10);
+  SigReceiver sig3(*item3);
+  mixer.PushToFront(item3);
+
+  {
+  EXPECT_CALL(sig1, MouseOwnerChanged(_)).Times(0);
+  EXPECT_CALL(sig2, MouseOwnerChanged(_)).Times(0);
+  EXPECT_CALL(sig3, MouseOwnerChanged(true));
+
+  mixer.EnterEvent(CompPoint(5, 5));
+
+  ASSERT_TRUE(item3->mouse_owner());
+  ASSERT_EQ(item3, mixer.GetMouseOwner());
+  }
+
+  {
+  EXPECT_CALL(sig1, MouseOwnerChanged(_)).Times(0);
+  EXPECT_CALL(sig2, MouseOwnerChanged(true));
+  EXPECT_CALL(sig3, MouseOwnerChanged(false));
+
+  mixer.EnterEvent(CompPoint(15, 15));
+
+  ASSERT_FALSE(item3->mouse_owner());
+  ASSERT_TRUE(item2->mouse_owner());
+  ASSERT_EQ(item2, mixer.GetMouseOwner());
+  }
+
+  {
+  EXPECT_CALL(sig1, MouseOwnerChanged(true));
+  EXPECT_CALL(sig2, MouseOwnerChanged(false));
+  EXPECT_CALL(sig3, MouseOwnerChanged(_)).Times(0);
+
+  mixer.EnterEvent(CompPoint(25, 25));
+
+  ASSERT_FALSE(item2->mouse_owner());
+  ASSERT_TRUE(item1->mouse_owner());
+  ASSERT_EQ(item1, mixer.GetMouseOwner());
+  }
+}
+
+TEST_F(TestDecorationInputMixer, MotionEvent)
+{
+  auto item1 = SizedMockItem(30, 30);
+  SigReceiver sig1(*item1);
+  mixer.PushToFront(item1);
+
+  auto item2 = SizedMockItem(20, 20);
+  SigReceiver sig2(*item2);
+  mixer.PushToFront(item2);
+
+  auto item3 = SizedMockItem(10, 10);
+  SigReceiver sig3(*item3);
+  mixer.PushToFront(item3);
+
+  {
+  EXPECT_CALL(sig1, MouseOwnerChanged(_)).Times(0);
+  EXPECT_CALL(sig2, MouseOwnerChanged(_)).Times(0);
+  EXPECT_CALL(sig3, MouseOwnerChanged(true));
+  EXPECT_CALL(*item1, MotionEvent(_)).Times(0);
+  EXPECT_CALL(*item2, MotionEvent(_)).Times(0);
+  EXPECT_CALL(*item3, MotionEvent(CompPoint(5, 5)));
+
+  mixer.MotionEvent(CompPoint(5, 5));
+
+  ASSERT_TRUE(item3->mouse_owner());
+  ASSERT_EQ(item3, mixer.GetMouseOwner());
+  }
+
+  {
+  EXPECT_CALL(sig1, MouseOwnerChanged(_)).Times(0);
+  EXPECT_CALL(sig2, MouseOwnerChanged(true));
+  EXPECT_CALL(sig3, MouseOwnerChanged(false));
+  EXPECT_CALL(*item1, MotionEvent(_)).Times(0);
+  EXPECT_CALL(*item2, MotionEvent(CompPoint(15, 15)));
+  EXPECT_CALL(*item3, MotionEvent(_)).Times(0);
+
+  mixer.MotionEvent(CompPoint(15, 15));
+
+  ASSERT_FALSE(item3->mouse_owner());
+  ASSERT_TRUE(item2->mouse_owner());
+  ASSERT_EQ(item2, mixer.GetMouseOwner());
+  }
+
+  {
+  EXPECT_CALL(sig1, MouseOwnerChanged(true));
+  EXPECT_CALL(sig2, MouseOwnerChanged(false));
+  EXPECT_CALL(sig3, MouseOwnerChanged(_)).Times(0);
+  EXPECT_CALL(*item1, MotionEvent(CompPoint(25, 25)));
+  EXPECT_CALL(*item2, MotionEvent(_)).Times(0);
+  EXPECT_CALL(*item3, MotionEvent(_)).Times(0);
+
+  mixer.MotionEvent(CompPoint(25, 25));
+
+  ASSERT_FALSE(item2->mouse_owner());
+  ASSERT_TRUE(item1->mouse_owner());
+  ASSERT_EQ(item1, mixer.GetMouseOwner());
+  }
 }
 
 }
