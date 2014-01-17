901a4bdfaf7e3d553c1c2fe55f5c001cb7926247
Brandon Schaefer <brandontschaefer@gmail.com>
2014-01-17 14:01:27 -0800

    * Fix merged problems
    (bzr r3625.1.11)

--- unity-shared/EMConverter.cpp
+++ unity-shared/EMConverter.cpp
@@ -102,20 +102,4 @@ double EMConverter::DPIScale() const
   return dpi_ / BASE_DPI;
 }
 
-double EMConverter::PixelsToBaseEM(int pixels) const
-{
-  return (pixels / base_pixels_per_em_);
-}
-
-int EMConverter::ConvertPixels(int pixels) const
-{
-  double pixels_em = PixelsToBaseEM(pixels);
-  return EMToPixels(pixels_em);
-}
-
-double EMConverter::DPIScale() const
-{
-  return dpi_ / BASE_DPI;
-}
-
 } // namespace unity
