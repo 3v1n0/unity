d02e08209df4f93c73d7f199826ac5ac9cdf8b31
Marco Trevisan (Treviño) <mail@3v1n0.net>
2014-02-12 08:09:43 +0100

    MenuManager: add a wrapper for Indicators and KeyGrabber that registers mnemonics
    It also emits signals with accumulators that will just stop the emission when a client
    returns true.
    
    (bzr r3652.3.2)

---
+++ unity-shared/MenuManager.h
@@ -0,0 +1,69 @@
+// -*- Mode: C++; indent-tabs-mode: nil; tab-width: 2 -*-
+/*
+ * Copyright (C) 2014 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Marco Trevisan <marco.trevisan@canonical.com>
+ */
+
+#ifndef __UNITY_INDICATOR_MANAGER__
+#define __UNITY_INDICATOR_MANAGER__
+
+#include <NuxCore/Property.h>
+#include <UnityCore/Indicators.h>
+#include "KeyGrabber.h"
+
+namespace unity
+{
+namespace menu
+{
+
+struct any_true
+{
+  typedef bool result_type;
+  template <class IT> result_type operator()(IT it, IT end)
+  {
+    for (; it != end; ++it) if (*it) return true;
+    return false;
+  }
+};
+
+class Manager : public sigc::trackable
+{
+public:
+  typedef std::shared_ptr<Manager> Ptr;
+
+  nux::Property<bool> show_menus;
+
+  Manager(indicator::Indicators::Ptr const&, key::Grabber::Ptr const&);
+  virtual ~Manager();
+
+  indicator::Indicators::Ptr const& Indicators() const;
+  key::Grabber::Ptr const& KeyGrabber() const;
+
+  sigc::signal<bool>::accumulated<any_true> open_first;
+  sigc::signal<bool, std::string const&>::accumulated<any_true> activate_entry;
+
+private:
+  Manager(Manager const&) = delete;
+  Manager& operator=(Manager const&) = delete;
+
+  struct Impl;
+  std::unique_ptr<Impl> impl_;
+};
+
+} // menu namespace
+} // unity namespace
+
+#endif // __UNITY_INDICATOR_MANAGER__
