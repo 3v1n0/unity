e0fc8cccbc1d087985ab9df72772d5529a096b8c
Marco Trevisan (Treviño) <mail@3v1n0.net>
2014-02-24 19:21:46 +0100

    PluginAdapter: a window is on a given monitor, when it's center is on that
    (bzr r3680.1.19)

--- unity-shared/PluginAdapter.cpp
+++ unity-shared/PluginAdapter.cpp
@@ -419,7 +419,7 @@ int PluginAdapter::MonitorGeometryIn(nux::Geometry const& geo) const
   {
     nux::Geometry const& i_g = geo.Intersect(monitors[i]);
 
-    if (i_g.width > 0 && i_g.height > 0)
+    if (i_g.width > geo.width/2 && i_g.height > geo.height/2)
       return i;
   }
 
