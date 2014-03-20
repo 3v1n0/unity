5cfc7999b1a6ba835878cdffed02250bfa22cda7
Brandon Schaefer <brandontschaefer@gmail.com>
2014-03-19 17:11:08 -0700

    * Cache the x11 display
    (bzr r3731.2.3)

--- launcher/LauncherController.cpp
+++ launcher/LauncherController.cpp
@@ -1253,13 +1253,14 @@ void Controller::HandleLauncherKeyRelease(bool was_tap, int when)
 
 bool Controller::HandleLauncherKeyEvent(unsigned long key_state, unsigned int key_sym, Time timestamp)
 {
+  Display* display = nux::GetGraphicsDisplay()->GetX11Display();
   // Turn the key_sym back to a keycode, this turns keypad key_sym to the correct top row key_code
-  unsigned int key_code = XKeysymToKeycode(nux::GetGraphicsDisplay()->GetX11Display(), key_sym);
+  unsigned int key_code = XKeysymToKeycode(display, key_sym);
 
   // Shortcut to start launcher icons. Only relies on Keycode, ignore modifier
   for (auto const& icon : *pimpl->model_)
   {
-    unsigned int shortcut_code = XKeysymToKeycode(nux::GetGraphicsDisplay()->GetX11Display(), icon->GetShortcut());
+    unsigned int shortcut_code = XKeysymToKeycode(display, icon->GetShortcut());
     if (shortcut_code == key_code)
     {
       if ((key_state & nux::KEY_MODIFIER_SHIFT) &&
