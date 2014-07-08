004b73bfa499839005ce361af4218f0279711c5e
Marco Trevisan (Treviño) <mail@3v1n0.net>
2014-07-08 18:50:54 +0200

    Previews: add support for --scaling-factor=X.Y parameter to standalone versions
    (bzr r3830.2.25)

--- dash/previews/StandaloneSocialPreview.cpp
+++ dash/previews/StandaloneSocialPreview.cpp
@@ -38,9 +38,10 @@
 #include "Preview.h"
 #include "PreviewContainer.h"
 
+const unity::RawPixel WIDTH(1000);
+const unity::RawPixel HEIGHT(600);
 
-#define WIDTH 910 
-#define HEIGHT 400
+static double scale = 1.0;
 
 using namespace unity;
 using namespace unity::dash;
@@ -146,6 +147,7 @@ void TestRunner::Init ()
   container_->navigate_right.connect(sigc::mem_fun(this, &TestRunner::NavRight));
   container_->navigate_left.connect(sigc::mem_fun(this, &TestRunner::NavLeft));
   container_->request_close.connect([this]() { exit(0); });
+  container_->scale = scale;
 
   DummyView* dummyView = new DummyView(container_.GetPointer());
   layout_ = new nux::VLayout(NUX_TRACKER_LOCATION);
@@ -269,10 +271,22 @@ int main(int argc, char **argv)
   unity::dash::previews::Style panel_style;
   unity::dash::Style dash_style;
   unity::ThumbnailGenerator thumbnail_generator;
+  unity::glib::Error err;
+
+  GOptionEntry args_parsed[] =
+  {
+    { "scaling-factor", 's', 0, G_OPTION_ARG_DOUBLE, &scale, "The dash scaling factor", "F" },
+    { NULL }
+  };
+
+  std::shared_ptr<GOptionContext> ctx(g_option_context_new("Unity Preview"), g_option_context_free);
+  g_option_context_add_main_entries(ctx.get(), args_parsed, NULL);
+  if (!g_option_context_parse(ctx.get(), &argc, &argv, &err))
+    std::cerr << "Got error when parsing arguments: " << err << std::endl;
 
   TestRunner *test_runner = new TestRunner ();
   wt = nux::CreateGUIThread(TEXT("Unity Preview"),
-                            WIDTH, HEIGHT,
+                            WIDTH.CP(scale), HEIGHT.CP(scale),
                             0,
                             &TestRunner::InitWindowThread,
                             test_runner);
