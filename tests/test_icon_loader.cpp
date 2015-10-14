491eed241917726e70a7db89a3eb47426711fba0
Marco Trevisan (Treviño) <mail@3v1n0.net>
2015-10-14 11:32:50 +0100

    TestIconLoader: wait pending events before on TearDown
    (bzr r4008.9.3)

--- tests/test_icon_loader.cpp
+++ tests/test_icon_loader.cpp
@@ -90,6 +90,8 @@ struct TestIconLoader : testing::Test
   {
     for (auto handle : handles_)
       icon_loader.DisconnectHandle(handle);
+
+    Utils::WaitPendingEvents();
   }
 
   IconLoader& icon_loader;
