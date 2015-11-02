a036088d2efe10c8ce969945611ba56b8e57687a
handsome_feng <445865575@qq.com>
2015-11-02 15:15:11 +0800

    modified the login icon
    (bzr r4016.2.20)

--- unity-shared/TextInput.cpp
+++ unity-shared/TextInput.cpp
@@ -54,6 +54,9 @@ const RawPixel TOOLTIP_Y_OFFSET  =  3_em;
 const RawPixel TOOLTIP_OFFSET    = 10_em;
 const RawPixel DEFAULT_ICON_SIZE = 22_em;
 
+const RawPixel LOGIN_ICON_WIDTH  = 32_em;
+const RawPixel LOGIN_ICON_HEIGHT = 62_em;
+
 std::string WARNING_ICON    = "dialog-warning-symbolic";
 // Fonts
 const std::string HINT_LABEL_DEFAULT_FONT_NAME = "Ubuntu";
@@ -259,11 +262,42 @@ void TextInput::UpdateHintFont()
 
 nux::ObjectPtr<nux::BaseTexture> TextInput::LoadActivatorIcon(int icon_size)
 {
-  TextureCache& cache = TextureCache::GetDefault();
   if (g_strcmp0(getenv("KYLIN_CURRENT_DESKTOP"), "Kylin") == 0)
-    return cache.FindTexture("login.png", 62, 36);
+  {
+    glib::Error error;
+    glib::Object<GdkPixbuf> pixbuf(gdk_pixbuf_new_from_file_at_size(PKGDATADIR"/login.png", LOGIN_ICON_WIDTH, LOGIN_ICON_HEIGHT, &error));
+
+    if (pixbuf != nullptr)
+    {
+        nux::CairoGraphics cg(CAIRO_FORMAT_ARGB32, gdk_pixbuf_get_width(pixbuf), gdk_pixbuf_get_height(pixbuf));
+        cairo_t* cr = cg.GetInternalContext();
+        cairo_text_extents_t et;
+        const std::string login = _("login");
+        double x, y;
+
+        gdk_cairo_set_source_pixbuf(cr, pixbuf, 0, 0);
+        cairo_paint(cr);
+        cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
+        cairo_set_font_size(cr, 18);
+        cairo_set_source_rgba(cr, 1.0f, 1.0f, 1.0f, 1.0f);
+
+        cairo_text_extents(cr, login.c_str(), &et);
+        x = LOGIN_ICON_WIDTH/2 - (et.width/2 + et.x_bearing);
+        y = LOGIN_ICON_HEIGHT/2 - (et.height/2 + et.y_bearing);
+        cairo_move_to(cr, x, y);
+        cairo_show_text(cr, login.c_str());
+
+        return texture_ptr_from_cairo_graphics(cg);
+    } else {
+        TextureCache& cache = TextureCache::GetDefault();
+        return cache.FindTexture("arrow_right.png", icon_size, icon_size);
+    }
+  }
   else
+  {
+    TextureCache& cache = TextureCache::GetDefault();
     return cache.FindTexture("arrow_right.png", icon_size, icon_size);
+  }
 }
 
 nux::ObjectPtr<nux::BaseTexture> TextInput::LoadWarningIcon(int icon_size)
