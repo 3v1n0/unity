02aed21d5a8a2e7695e5cb66b35ced6a8c3543a8
Marco Trevisan (Treviño) <mail@3v1n0.net>
2015-11-03 13:45:05 +0100

    SwitcherModel: avoid signed/unsigned comparisons
    (bzr r4027.5.21)

--- launcher/SwitcherModel.cpp
+++ launcher/SwitcherModel.cpp
@@ -105,7 +105,7 @@ void SwitcherModel::VerifyApplications()
   {
     if (!(*it)->ShowInSwitcher(only_apps_on_viewport))
     {
-      auto icon_index = it - applications_.begin();
+      unsigned icon_index = it - applications_.begin();
       hidden_applications_.push_back(*it);
       it = applications_.erase(it);
       anything_changed = true;
@@ -150,7 +150,7 @@ void SwitcherModel::InsertIcon(AbstractLauncherIcon::Ptr const& application)
   if (sort_by_priority_)
   {
     auto pos = std::upper_bound(applications_.begin(), applications_.end(), application, CompareSwitcherItemsPriority);
-    auto icon_index = pos - applications_.begin();
+    unsigned icon_index = pos - applications_.begin();
     applications_.insert(pos, application);
 
     if (icon_index <= index_)
@@ -197,7 +197,7 @@ void SwitcherModel::RemoveIcon(launcher::AbstractLauncherIcon::Ptr const& icon)
   auto icon_it = std::find(applications_.begin(), applications_.end(), icon);
   if (icon_it != applications_.end())
   {
-    auto icon_index = icon_it - applications_.begin();
+    unsigned icon_index = icon_it - applications_.begin();
     bool was_in_detail = (detail_selection && icon_index == index_);
     applications_.erase(icon_it);
 
