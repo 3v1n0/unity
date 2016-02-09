96be83f07e2081d2346a11f44fdcaafac0cb1916
Marco Trevisan (Treviño) <mail@3v1n0.net>
2016-02-09 20:30:37 +0100

    GtkUtils: add gtk::Setting class for monitoring and getting settings
    (bzr r4068.4.10)

--- unity-shared/GtkUtils.h
+++ unity-shared/GtkUtils.h
@@ -21,7 +21,10 @@
 #define UNITY_GTK_UTILS
 
 #include <gtk/gtk.h>
+#include <sigc++/sigc++.h>
+
 #include <UnityCore/GLibWrapper.h>
+#include <UnityCore/GLibSignal.h>
 
 namespace unity
 {
@@ -41,6 +44,28 @@ inline std::string GetSettingValue(std::string const& name)
 {
   return glib::String(gtk::GetSettingValue<gchar*>(name)).Str();
 }
+
+template <typename TYPE>
+class Setting
+{
+public:
+  Setting(std::string const& name)
+    : name_(name)
+  {
+    connection_.Connect(gtk_settings_get_default(), "notify::"+name_, [this] (GtkSettings*, GParamSpec*) {
+      changed.emit(GetSettingValue<TYPE>(name_));
+    });
+  }
+
+  TYPE Value() const { return GetSettingValue<TYPE>(name_); }
+  TYPE operator()() const { return Value(); }
+
+  sigc::signal<void, TYPE const&> changed;
+
+private:
+  std::string name_;
+  glib::Signal<void, GtkSettings*, GParamSpec*> connection_;
+};
 
 } // theme namespace
 } // unity namespace
