79d587ad0d2dfc3a647a0fd4de708e2cb8279818
Marco Trevisan (Treviño) <mail@3v1n0.net>
2016-05-11 01:03:20 +0200

    UnityShell: fix header license
    (bzr r4105.7.2)

--- plugins/unityshell/src/unityshell.h
+++ plugins/unityshell/src/unityshell.h
@@ -2,12 +2,12 @@
 /* Compiz unity plugin
  * unity.h
  *
- * Copyright(c) 2010-11 Canonical Ltd.
+ * Copyright (c) 2010-16 Canonical Ltd.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 3
- * of the License, or(at your option) any later version.
+ * of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
