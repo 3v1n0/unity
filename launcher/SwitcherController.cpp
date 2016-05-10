a69b1ba109f24eb3a55db54c76bcb831d8686aee
Marco Trevisan (Treviño) <mail@3v1n0.net>
2016-05-10 17:01:05 +0200

    SwitcherController: don't try to get a window from empty selections
    Fixes LP: #1580211
    
    (bzr r4105.8.1)

--- launcher/SwitcherController.cpp
+++ launcher/SwitcherController.cpp
@@ -690,7 +690,10 @@ Selection Controller::Impl::GetCurrentSelection() const
       }
       else if (model_->SelectionIsActive())
       {
-        window = model_->SelectionWindows().front();
+        auto const& selection_windows = model_->SelectionWindows();
+
+        if (!selection_windows.empty())
+          window = selection_windows.front();
       }
     }
   }
