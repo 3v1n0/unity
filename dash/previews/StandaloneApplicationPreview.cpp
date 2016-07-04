03651257fc7a58bf4ede79d548ccb0fb4743b111
Marco Trevisan (Treviño) <mail@3v1n0.net>
2016-07-04 18:35:14 +0200

    PreviewContainer: use nux::Animation for handling resizing animation
    (bzr r4132.6.9)

--- dash/previews/StandaloneApplicationPreview.cpp
+++ dash/previews/StandaloneApplicationPreview.cpp
@@ -21,10 +21,12 @@
 #include "config.h"
 
 #include "Nux/Nux.h"
+#include "Nux/NuxTimerTickSource.h"
 #include "Nux/VLayout.h"
 #include "Nux/WindowThread.h"
 #include "NuxGraphics/GraphicsEngine.h"
 #include <Nux/Layout.h>
+#include <NuxCore/AnimationController.h>
 #include <NuxCore/Logger.h>
 #include <UnityCore/Variant.h>
 #include <UnityCore/ApplicationPreview.h>
@@ -340,6 +342,9 @@ int main(int argc, char **argv)
                             0,
                             &TestRunner::InitWindowThread,
                             test_runner);
+
+  nux::NuxTimerTickSource tick_source;
+  nux::animation::AnimationController animation_controller(tick_source);
 
   wt->Run (NULL);
   delete wt;
