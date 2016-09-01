58a15983dc6fc925e9df35d30407aae336e988e4
Marco Trevisan (Treviño) <mail@3v1n0.net>
2016-09-01 18:48:38 +0200

    UpstartWrapper: use ligther proxies
    (bzr r4177.4.9)

--- unity-shared/UpstartWrapper.cpp
+++ unity-shared/UpstartWrapper.cpp
@@ -42,8 +42,12 @@ private:
 
 UpstartWrapper::Impl::Impl(bool test_mode)
 {
+  auto flags = static_cast<GDBusProxyFlags>(G_DBUS_PROXY_FLAGS_DO_NOT_LOAD_PROPERTIES |
+                                            G_DBUS_PROXY_FLAGS_DO_NOT_CONNECT_SIGNALS);
+
   upstart_proxy_ = std::make_shared<unity::glib::DBusProxy>(test_mode ?  "com.canonical.Unity.Test.Upstart" : DBUS_SERVICE_UPSTART,
-                                                            DBUS_PATH_UPSTART, DBUS_INTERFACE_UPSTART);
+                                                            DBUS_PATH_UPSTART, DBUS_INTERFACE_UPSTART,
+                                                            G_BUS_TYPE_SESSION, flags);
 }
 
 void UpstartWrapper::Impl::Emit(std::string const& name)
